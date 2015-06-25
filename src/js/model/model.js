9d65ece621cd2a5dcbf1bbac36d08e67614e960d
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-06-25 19:18:50 +0900

    ADD: The model mixin module.

--- src/js/model/model.js
+++ src/js/model/model.js
@@ -1,54 +1,124 @@
 /**
- * @fileoverview Mixin module for models.
+ * @fileoverview Mixin module for models. it provide simple model validator.
  * @author NHN Ent. FE Development Team <e0242@nhnent.com>
  */
 'use strict';
 
 var util = global.ne.util,
     spaceRx = /^\s*|\s*$/g,
-    validator,
     model;
 
+var moment = require('moment');
+var isMoment = moment.isMoment;
+
+/**
+ * string trim
+ * @param {string} str string to trim
+ * @returns {string} trimed string
+ */
 function trim(str) {
     return str.replace(spaceRx, '');
 }
 
-validator = {
-    required: function(instance, fieldName) {
-        var target = instance[fieldName];
-
-        return !util.isUndefined(target) && trim(target) !== '';
-    }
-};
-
 /**
+ * Mixin module for models.
+ * @module ne.dooray.calendar.Model
  * @mixin
  */
 model = {
+    /**
+     * The collections of validator functions.
+     */
+    validators: {
+        /**
+         * check all of supplied fields(property) is not undefined or empty string.
+         * @param {object} instance model instance.
+         * @param {string[]} fields property names to check.
+         * @returns {boolean} return true when supplied fields are not undefined or empty string.
+         */
+        required: function(instance, fields) {
+            var valid = true,
+                isValid = function(obj) {
+                    return !util.isUndefined(obj) && trim(obj) !== '';
+                };
+
+            util.forEach(fields, function(fieldName) {
+                valid = isValid(instance[fieldName]);
+                return valid;
+            });
+
+            return valid;
+        },
+
+        /**
+         * check supplied fields are valid dates and valid date ranges.
+         * @param {object} instance model instance.
+         * @param {moment[]} fields array of date range (starts, ends)
+         * @returns {boolean} is valid date range?
+         */
+        dateRange: function(instance, fields) {
+            var starts,
+                ends;
+
+            if (!util.isExisty(instance) || fields.length !== 2) {
+                return true;
+            }
+
+            starts = instance[fields[0]];
+            ends = instance[fields[1]];
+
+            if (!isMoment(starts) || !isMoment(ends)) {
+                return false;
+            }
+
+            if (starts.isAfter(ends)) {
+                return false;
+            }
+
+            return true;
+        }
+    },
+
+    /**
+     * Check validate for model instance.
+     *
+     * The validate are works on a basis of constructor's "schema" property.
+     *
+     * You can customize validators add some method to model#validators.
+     * @returns {Boolean} model is valid?
+     */
     isValid: function() {
         var that = this,
             schema = this.constructor.schema,
+            validators = model.validators,
+            validator,
             valid = true;
 
         if (!schema) {
             return true;
         }
 
-        util.forEach(schema, function(fields, validatorName) {
-            util.forEach(fields, function(fieldName) {
-                if (validator[validatorName](that, fieldName) === false) {
-                    valid = false;
-                    return false;
-                }
-            });
+        util.forEach(schema, function(values, validatorName) {
+            validator = validators[validatorName];
 
-            if (valid === false) {
-                return false;
+            if (validator) {
+                valid = validator(that, values);
+                return valid;
             }
         });
 
         return valid;
     },
+
+    /**
+     * Mixin model module to supplied target.
+     * @param {Object} target The object of want to mixed.
+     * @example
+     * function Man() {
+     *     this.name = 'john';
+     * }
+     * model.mixin(Man.prototype);
+     */
     mixin: function(target) {
         util.forEach(model, function(method, name) {
             if (name !== 'mixin') {
@@ -59,3 +129,4 @@ model = {
 };
 
 module.exports = model;
+
--- test/model/model.spec.js
+++ test/model/model.spec.js
@@ -1,3 +1,91 @@
 /*eslint-disable*/
+var model = ne.dooray.calendar.Model;
+
 describe('model/model', function() {
+    var myObj;
+
+    beforeEach(function() {
+        myObj = {};
+    });
+
+    describe('validators', function() {
+        describe('require()', function() {
+            it('return false when supplied property is undefined.', function() {
+                expect(model.validators.required(myObj, ['good'])).toBe(false);
+            });
+
+            it('return false when supplied property is empty string', function() {
+                myObj.title = '';
+                expect(model.validators.required(myObj, ['title'])).toBe(false);
+                myObj.title = 'title';
+                expect(model.validators.required(myObj, ['title'])).toBe(true);
+            });
+
+            it('return true when supplied property are wrong', function() {
+                expect(model.validators.required(null, [])).toBe(true);
+            });
+        });
+
+        describe('dateRange()', function() {
+            it('return true when supplied property are wrong', function() {
+                expect(model.validators.dateRange(null, ['', ''])).toBe(true);
+                expect(model.validators.dateRange({}, [])).toBe(true);
+            });
+
+            it('return false when one of supplied property is not Moment object.', function() {
+                
+                myObj = {
+                    starts: moment('2015-05-01'),
+                    ends: moment('2015-05-02')
+                };
+
+                expect(model.validators.dateRange(myObj, ['starts', 'ends'])).toBe(true);
+
+                myObj.ends = null;
+
+                expect(model.validators.dateRange(myObj, ['starts', 'ends'])).toBe(false);
+            });
+
+            it('return true only starts and ends range are valid.', function() {
+                myObj = {
+                    starts: moment('2015-05-03'),
+                    ends: moment('2015-05-02')
+                };
+
+                expect(model.validators.dateRange(myObj, ['starts', 'ends'])).toBe(false);
+            });
+        });
+    });
+
+    describe('isValid()', function() {
+        beforeEach(function() {
+            myObj.constructor = {
+                schema: {
+                    required: ['title']
+                }
+            };
+        });
+
+        it('return true when schema is empty', function() {
+            expect(model.isValid.apply({})).toBe(true);
+        });
+
+        it('invoke each validator on a basis of constructor\'s schema property.', function() {
+            spyOn(model.validators, 'required');
+
+            myObj.title = 'name';
+
+            model.isValid.apply(myObj);
+
+            expect(model.validators.required).toHaveBeenCalledWith(myObj, ['title']);
+        });
+
+        it('skip validate when descibed validator on schema is not exist.', function() {
+            delete myObj.constructor.schema.required;
+            myObj.constructor.schema.notexist = ['good'];
+
+            expect(model.isValid.apply(myObj)).toBe(true);
+        });
+    });
 });
+
