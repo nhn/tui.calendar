8a26983ac41831e472d8ec134844f0fa6be0b578
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-06-26 11:08:46 +0900

    ADD: collection model.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ require('code-snippet');
 require('moment');
 var Event = require('./src/js/model/event');
 var Model = require('./src/js/model/model');
+var Colleciton = require('./src/js/model/collection');
 ne.util.defineNamespace('ne.dooray.calendar', {
     Model: Model,
+    Collection: Colleciton,
     Event: Event
 });
---
+++ src/js/model/collection.js
@@ -0,0 +1,228 @@
+/**
+ * @fileoverview Common collections.
+ * @author NHN Ent. FE Development Team <e0242@nhnent.com>
+ */
+'use strict';
+
+var util = global.ne.util,
+    forEachProp = util.forEachOwnProperties,
+    forEachArr = util.forEachArray,
+    isFunc = util.isFunction,
+    isObj = util.isObject;
+
+var aps = Array.prototype.slice;
+
+/**
+ * Common collection.
+ *
+ * It need function for get model's unique id.
+ *
+ * if the function is not supplied then it use default function {@link Collection#getItemID}
+ * @constructor
+ * @param {function} [getItemIDFn] function for get model's id.
+ */
+function Collection(getItemIDFn) {
+    /**
+     * @type {object.<string, *>}
+     */
+    this.items = {};
+
+    /**
+     * @type {number}
+     */
+    this.length = 0;
+
+    if (isFunc(getItemIDFn)) {
+        /**
+         * @type {function}
+         */
+        this.getItemID = getItemIDFn;
+    }
+}
+
+/**
+ * get model's unique id.
+ * @param {object} item model instance.
+ * @returns {number} model unique id.
+ */
+Collection.prototype.getItemID = function(item) {
+    return item._id + '';
+};
+
+/**
+ * add models.
+ * @param {...*} item models to add this collection.
+ */
+Collection.prototype.add = function(item) {
+    var id,
+        ownItems;
+
+    if (arguments.length > 1) {
+        forEachArr(aps.call(arguments), function(o) {
+            this.add(o);
+        }, this);
+
+        return;
+    }
+
+    id = this.getItemID(item);
+    ownItems = this.items;
+
+    if (!ownItems[id]) {
+        this.length += 1;
+    }
+    ownItems[id] = item;
+};
+
+/**
+ * remove models.
+ * @param {...(object|string|number)} id model instance or unique id to delete.
+ * @returns {array} deleted model list.
+ */
+Collection.prototype.remove = function(id) {
+    var removed = [],
+        ownItems,
+        itemToRemove;
+
+    if (!this.length) {
+        return removed;
+    }
+
+    if (arguments.length > 1) {
+        removed = [];
+        forEachArr(aps.call(arguments), function(id) {
+            removed.push(this.remove(id));
+        }, this);
+
+        return removed;
+    }
+
+    ownItems = this.items;
+
+    if (isObj(id)) {
+        id = this.getItemID(id);
+    }
+
+    if (!ownItems[id]) {
+        return removed;
+    }
+
+    this.length -= 1;
+    itemToRemove = ownItems[id];
+    delete ownItems[id];
+
+    return itemToRemove;
+};
+
+/**
+ * remove all models in collection.
+ */
+Collection.prototype.clear = function() {
+    this.items = {};
+    this.length = 0;
+};
+
+/**
+ * check collection has specific model.
+ * @param {(object|string|number|function)} id model instance or id or filter function to check
+ * @returns {boolean} is has model?
+ */
+Collection.prototype.has = function(id) {
+    var isFilter,
+        has;
+
+    if (!this.length) {
+        return false;
+    }
+
+    isFilter = isFunc(id);
+    has = false;
+
+    if (isFilter) {
+        this.each(function(item) {
+            if (id(item) === true) {
+                has = true;
+                return false;
+            }
+        });
+    } else {
+        id = isObj(id) ? this.getItemID(id) : id;
+        has = util.isExisty(this.items[id]);
+    }
+
+    return has;
+};
+
+/**
+ * invoke callback when model exist in collection.
+ * @param {(string|number)} id model unique id.
+ * @param {function} fn the callback.
+ * @param {*} [context] callback context.
+ */
+Collection.prototype.doWhenHas = function(id, fn, context) {
+    var item = this.items[id];
+
+    if (!util.isExisty(item)) {
+        return;
+    }
+
+    fn.call(context || this, item);
+};
+
+/**
+ * Search model. and return new collection.
+ * @param {function} filter filter function.
+ * @returns {Collection} new collection with filtered models.
+ * @example
+ * collection.find(function(item) {
+ *     return item.edited === true;
+ * });
+ */
+Collection.prototype.find = function(filter) {
+    var result = new Collection();
+
+    if (this.hasOwnProperty('getItemID')) {
+        result.getItemID = this.getItemID;
+    }
+
+    this.each(function(item) {
+        if (filter(item) === true) {
+            result.add(item);
+        }
+    });
+
+    return result;
+};
+
+/**
+ * sort a basis of supplied compare function.
+ * @param {function} compareFunction compareFunction
+ * @returns {array} sorted array.
+ */
+Collection.prototype.sort = function(compareFunction) {
+    var arr = [];
+
+    this.each(function(item) {
+        arr.push(item);
+    });
+
+    if (isFunc(compareFunction)) {
+        arr = arr.sort(compareFunction);
+    }
+
+    return arr;
+};
+
+/**
+ * iterate each model element.
+ *
+ * when iteratee return false then break the loop.
+ * @param {function} iteratee iteratee(item, index, items)
+ * @param {*} [context] context
+ */
+Collection.prototype.each = function(iteratee, context) {
+    forEachProp(this.items, iteratee, context || this);
+};
+
+module.exports = Collection;
+
