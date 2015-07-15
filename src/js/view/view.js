34a671029d45f470c0c602acf4e35b287acd7c29
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-07-15 23:09:09 +0900

    FIX: change view's base container element creation.

--- src/js/view/view.js
+++ src/js/view/view.js
@@ -5,23 +5,38 @@
 'use strict';
 
 var util = global.ne.util;
+var domutil = require('../common/domutil');
 
 /**
+ * Base class of views.
+ *
+ * All views create own container element inside supplied container element.
  * @constructor
  * @param {options} options The object for describe view's specs.
  * @param {HTMLElement} container Default container element for view. you can use this element for this.container syntax.
  */
 function View(options, container) {
+    var id = util.stamp(this);
+
     options = options || {};
 
-    if (util.isString(container)) {
-        container = global.document.getElementById(container);
+    if (util.isUndefined(container)) {
+        container = domutil.appendHTMLElement('div');
     }
 
-    if (!container) {
-        throw new Error('View(): need container element.');
-    }
+    container = domutil.appendHTMLElement('div', container);
+    container.setAttribute('id', 'view-' + id);
 
+    /**
+     * unique id
+     * @type {number}
+     */
+    this.id = id;
+
+    /**
+     * base element of view.
+     * @type {HTMLDIVElement}
+     */
     this.container = container;
 }
 
--- test/app/view/view.spec.js
+++ test/app/view/view.spec.js
@@ -1,4 +1,5 @@
 /*eslint-disable*/
+var util = ne.util;
 var View = ne.dooray.calendar.View;
 describe('View', function() {
     var view;
@@ -8,22 +9,15 @@ describe('View', function() {
     });
 
     describe('View()', function() {
-        it('throw error when container element not supplied.', function() {
-            expect(function() {
-                new View();
-            }).toThrow();
+        it('make an container element on body when container is not supplied.', function() {
+            view = new View();
+            expect(document.getElementById('view-' + util.stamp(view))).toBe(view.container);
         });
 
         it('setting default container.', function() {
             var el = document.getElementById('container');
             view = new View(null, el);
-            expect(view.container).toEqual(el);
-        });
-
-        it('can use container element id for constructor', function() {
-            var el = document.getElementById('container');
-            view = new View(null, 'container');
-            expect(view.container).toEqual(el);
+            expect(view.container).toEqual(el.childNodes[0]);
         });
     });
 });
