981cb01bf5849ecb4a3575abf5ebed01f554439e
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-07-22 14:10:20 +0900

    Add Week#_getRenderDateRange

--- src/js/view/week.js
+++ src/js/view/week.js
@@ -6,6 +6,7 @@
 
 var util = global.ne.util;
 var domutil = require('../common/domutil');
+var datetime = require('../datetime');
 var View = require('./view');
 
 /**********
@@ -15,22 +16,38 @@ var TimeGrid = require('./timeGrid');
 
 /**
  * @constructor
- * @param {Base.Days} controller The controller mixin part.
+ * @param {Base.Week} controller The controller mixin part.
+ * @param {object} options View options
+ * @param {Date} [options.renderStartDate] Start date of render. if not supplied then use -3d from today.
+ * @param {Date} [options.renderEndDate] End date of render. if not supplied then use +3d from today.
  * @param {HTMLElement} container The element to use container for this view.
  * @extends {View}
  */
-function Week(controller, container) {
+function Week(controller, options, container) {
+    var range;
+
     View.call(this, null, container);
+
+    range = this._getRenderDateRange(new Date());
+
+    /**
+     * @type {object} Options for view.
+     */
+    this.options = util.extend({
+        renderStartDate: range.start,
+        renderEndDate: range.end
+    }, options);
 
     /**
      * Week controller mixin.
-     * @type {Base.Days}
+     * @type {Base.Week}
      */
     this.controller = controller;
 
     domutil.addClass(container, 'view-days-container');
     domutil.disableTextSelection(container);
 
+    //TODO: add childs need to in service code (or factory)
     this.addChild(new TimeGrid(
         null,
         domutil.appendHTMLElement(
@@ -41,6 +58,29 @@ function Week(controller, container) {
 }
 
 util.inherit(Week, View);
+
+/**********
+ * Prototype props
+ **********/
+
+/**
+ * Calculate default render date range from supplied date.
+ * @param {Date} baseDate base date.
+ * @returns {object} date range.
+ */
+Week.prototype._getRenderDateRange = function(baseDate) {
+    var base = datetime.start(baseDate),
+        start = new Date(+base),
+        end = new Date(+base);
+
+    start.setDate(start.getDate() - 3);
+    end.setDate(end.getDate() + 3);
+
+    return {
+        start: start,
+        end: end
+    };
+};
 
 module.exports = Week;
 
--- test/app/view/week.spec.js
+++ test/app/view/week.spec.js
@@ -1,14 +1,37 @@
 /*eslint-disable*/
 var Week = ne.dooray.calendar.Week;
-describe('View/Days', function() {
+describe('View/Week', function() {
     var view;
 
     beforeEach(function() {
         loadFixtures('view.html');
-        view = new Week(null, document.getElementById('container2'));
+        view = new Week(null, null, document.getElementById('container2'));
     });
 
     it('Create child views', function() {
         expect(view.childs.length).not.toBe(0);
+    });
+
+    describe('_getRenderDateRange()', function() {
+        it('Calculate render dates from supplied date.', function() {
+            var d = new Date('2015-05-02T12:30:00+09:00');
+
+            var expectedStart = new Date(+d);
+            expectedStart.setDate(expectedStart.getDate() - 3);
+            expectedStart.setHours(0, 0, 0);
+            
+            var expectedEnd = new Date(+d);
+            expectedEnd.setDate(expectedEnd.getDate() + 3);
+            expectedEnd.setHours(0, 0, 0);
+
+            var expected = {
+                start: expectedStart,
+                end: expectedEnd
+            };
+
+            var result = view._getRenderDateRange(d);
+
+            expect(result).toEqual(expected);
+        });
     });
 });
