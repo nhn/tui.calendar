572c11af1dc6c1002ce8eef94b6db3137792af74
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-08-12 16:24:12 +0900

    Add ignore for definitions of interfaces

--- src/js/interface/handler.js
+++ src/js/interface/handler.js
@@ -4,19 +4,23 @@
  * Interface for event handler module for views.
  * @interface
  */
+/* istanbul ignore next */
 function Handler() {}
 
 /**
  * Destroy method
  */
+/* istanbul ignore next */
 Handler.prototype.destroy = function() {};
 
 /**
  * Connect view, dragHandler, controllers.
  */
+/* istanbul ignore next */
 Handler.prototype.connect = function() {};
 
 /**
  * Check handler has privilige for handle emitted events.
  */
+/* istanbul ignore next */
 Handler.prototype.checkExpectedCondition = function() {};
