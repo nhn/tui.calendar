9288e237aa58ba621e8bcb92c64a4756f481eeb9
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-09-23 11:11:58 +0900

    Change bundle configuration.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 /*eslint-disable*/
 var path = require('path');
 var gulp = require('gulp');
+var gutil = require('gulp-util');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var connect = require('gulp-connect');
 var browserify = require('browserify');
@@ -31,6 +35,8 @@ var HEADER = [
 ' */',
 ''].join('\n');
 
+var isProduction = gutil.env.production;
+
 gulp.task('default', function(done) {
     new KarmaServer({
         configFile: path.join(__dirname, 'karma.conf.js'),
@@ -54,13 +60,21 @@ gulp.task('bundle', function() {
     var tmpl = handlebars.compile(HEADER);
     var versionHeader = tmpl(pkg);
 
+    if (isProduction) {
+        gutil.log(gutil.colors.yellow('<< Bundling for Production >>'));
+    }
+
     gulp.src([
             'src/css/common.css',
             'src/css/*.css'
         ])
         .pipe(concat('calendar.css'))
         .pipe(insert.prepend(versionHeader))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(isProduction ? cssmin() : gutil.noop())
+        .pipe(isProduction ? rename({extname: '.min.css'}) : gutil.noop())
+        .pipe(insert.prepend(versionHeader))
+        .pipe(isProduction ? gulp.dest('dist') : gutil.noop());
 
     var b = browserify({
         entries: 'index.js',
@@ -87,9 +101,13 @@ gulp.task('bundle', function() {
             console.log(err.message);
             this.emit('end');
         })
-        .pipe(source('app.js'))
+        .pipe(source('calendar.js'))
         .pipe(buffer())
         .pipe(gulp.dest('dist'))
+        .pipe(isProduction ? uglify({compress:{}}) : gutil.noop())
+        .pipe(isProduction ? rename({extname: '.min.js'}) : gutil.noop())
+        .pipe(isProduction ? insert.prepend(versionHeader) : gutil.noop())
+        .pipe(isProduction ? gulp.dest('dist') : gutil.noop())
         .pipe(connect.reload());
 });
 
