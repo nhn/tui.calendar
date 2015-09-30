8733bdfdce0fc6bf08bfa5419a7dfb755fd3cbcd
MinHyeong-Kim <minhyeong.kim@nhnent.com>
2015-09-30 18:40:24 +0900

    service/minicalendar draft

--- src/js/config.js
+++ src/js/config.js
@@ -4,7 +4,17 @@
  */
 'use strict';
 
-module.exports = {
+var label = {
+    DAY_NAME: {
+        kor: ['일', '월', '화', '수', '목', '금', '토']
+    },
+    NEW_EVENT: {
+        kor: '새 일정'
+    }
+}
+
+var config = {
+    label: label,
     monthweek: {    // 종일일정
         view: {
             CONTAINER_PADDING_LEFT: 60,    // 이벤트 컨테이너 엘리먼트의 left padding.
@@ -13,14 +23,14 @@ module.exports = {
         },
         handler: {
             guide: {
-                TEXT_FOR_NEW_EVENT: '새 일정'    // 종일일정 생성 효과 엘리먼트 텍스트
+                TEXT_FOR_NEW_EVENT: label.NEW_EVENT.kor    // 종일일정 생성 효과 엘리먼트 텍스트
             }
         }
     },
     dayname: {    // dayname
         view: {
             DAY_NAME: {    // Date.day 인덱스에 해당하는 일자 레이블 텍스트
-                kor: ['일', '월', '화', '수', '목', '금', '토']
+                kor: label.DAY_NAME.kor
             }
         }
     },
@@ -31,5 +41,7 @@ module.exports = {
             SET_SCROLL_DELAY: global.ne.util.browser.msie ? 100 : 50
         }
     }
-};
+}
+
+module.exports = config;
 
--- src/js/service/view/minicalendar.js
+++ src/js/service/view/minicalendar.js
@@ -5,9 +5,45 @@
 'use strict';
 
 var util = global.ne.util;
+var config = require('../../config');
 var View = require('../../view/view');
 var domutil = require('../../common/domutil');
+var domevent = require('../../common/domevent');
 var datetime = require('../../datetime');
+var Handlebars = require('hbsfy/runtime');
+var tmpl = require('./minicalendar.hbs');
+
+Handlebars.registerHelper('minical-otherdate', function() {
+    if (this < 0) {
+        return 'schedule-view-minicalendar-other';
+    }
+});
+
+Handlebars.registerHelper('minical-abs', function() {
+    return Math.abs(this);
+});
+
+Handlebars.registerHelper('minical-header', function() {
+    var dayname = this.dayname,
+        index = this.startDayOfWeek;
+
+    return util.map(util.range(7), function() {
+        var tag = '<th>' + dayname[index] + '</th>';
+        index += 1;
+        if (index > 6) {
+            index = 0;
+        }
+        return tag;
+    }).join('');
+});
+
+Handlebars.registerHelper('minical-dots', function(eventsInDates) {
+    var events = eventsInDates[this];
+
+    if (events) {
+        return util.map(util.range(events.length), function() { return '&bull;'; }).join('');
+    }
+});
 
 /**
  * @constructor
@@ -19,24 +55,110 @@ var datetime = require('../../datetime');
  * @param {HTMLDivElement} container - element to use container
  */
 function MiniCalendar(options, container) {
-    var baseMonth;
+    var defaultMonth;
 
     View.call(this, options, container);
 
     domutil.addClass(container, 'schedule-view-minicalendar');
 
-    baseMonth = datetime.format(new Date(), 'YYYY-MM');
+
+    defaultMonth = util.pick(options, 'renderMonth');
+    if (defaultMonth) {
+        defaultMonth = datetime.parse(defaultMonth + '-01');
+    } else {
+        defaultMonth = new Date();
+    }
+
+    domevent.on(this.container, 'click', this._onClick, this);
 
     /**
      * @type {object}
      */
     this.options = util.extend({
-        startDayOfWeek: 0,
-        renderMonth: baseMonth
+        startDayOfWeek: 0
     }, options);
+
+    this.options.renderMonth = defaultMonth;
 }
 
 util.inherit(MiniCalendar, View);
+
+/**
+ * Next, Prev button event handler
+ * @param {HTMLButtonElement} buttonElement - next, prev button from _onClick event handler
+ */
+MiniCalendar.prototype.nav = function(buttonElement) {
+    var isNext = domutil.hasClass(buttonElement, 'schedule-view-minicalendar-next'),
+        options = this.options,
+        offset = isNext ? 1 : -1;
+
+    options.renderMonth.setMonth(options.renderMonth.getMonth() + offset);
+
+    this.render();
+};
+
+/**
+ * Date button event handler
+ * @param {HTMLButtonElement} buttonElement - date button from _onClick event handler
+ */
+MiniCalendar.prototype.date = function(buttonElement) {
+    var td = domutil.closest(buttonElement, 'td'),
+        previous;
+
+    if (td) {
+        previous = domutil.find('.schedule-view-minicalendar-focused', this.container);
+
+        if (previous) {
+            domutil.removeClass(previous, 'schedule-view-minicalendar-focused');
+        }
+
+        domutil.addClass(td, 'schedule-view-minicalendar-focused');
+    }
+};
+
+/**
+ * Click event handler
+ * @param {MouseEvent} clickEvent - click mouse event object
+ */
+MiniCalendar.prototype._onClick = function(clickEvent) {
+    var target = clickEvent.srcElement || clickEvent.target,
+        button = domutil.closest(target, 'button');
+
+    if (!button) {
+        return;
+    }
+
+    if (domutil.hasClass(button, 'schedule-view-minicalendar-date')) {
+        this.date(button);
+        return;
+    }
+
+    if (domutil.hasClass(button, 'schedule-view-minicalendar-nav')) {
+        this.nav(button);
+        return;
+    }
+};
+
+/**
+ * Render view
+ */
+MiniCalendar.prototype.render = function() {
+    var container = this.container,
+        options = this.options,
+        viewModel = {
+            month: options.renderMonth,
+            renderMonth: datetime.format(options.renderMonth, 'YYYY.MM'),
+            startDayOfWeek: options.startDayOfWeek,
+            dayname: config.label.DAY_NAME.kor,
+            eventsInDates: {
+                '-30': [{}, {}],
+                '4': [{}]
+            },
+            calendar: datetime.arr2dCalendar(options.renderMonth, options.startDayOfWeek)
+        };
+
+    container.innerHTML = tmpl(viewModel);
+};
 
 module.exports = MiniCalendar;
 
