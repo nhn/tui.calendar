c9d5a6ce289fde9885d9144233c043013f0eaec3
minhyeong.kim <minhyeong.kim@nhnent.com>
2015-10-28 11:21:26 +0900

    Add allday click handler module

---
+++ src/js/handler/allday/click.js
@@ -0,0 +1,85 @@
+/**
+ * @fileoverview Click handle module for allday events
+ * @author NHN Ent. FE Development Team <dl_javascript@nhnent.com>
+ */
+'use strict';
+
+var util = global.ne.util;
+var domutil = require('../../common/domutil');
+var AlldayMove = require('./move');
+
+/**
+ * @constructor
+ * @implements {Handler}
+ * @mixes CustomEvents
+ * @param {Drag} [dragHandler] - Drag handler instance.
+ * @param {Allday} [alldayView] - MonthWeek view instance.
+ * @param {Base} [baseController] - Base controller instance.
+ */
+function AlldayClick(dragHandler, alldayView, baseController) {
+    /**
+     * @type {Drag}
+     */
+    this.dragHandler = dragHandler;
+
+    /**
+     * @type {Allday}
+     */
+    this.alldayView = alldayView;
+
+    /**
+     * @type {Base}
+     */
+    this.baseController = baseController;
+
+    dragHandler.on({
+        'click': this._onClick
+    }, this);
+}
+
+/**
+ * Destroy handler module
+ */
+AlldayClick.prototype.destroy = function() {
+    this.dragHandler.off(this);
+    this.alldayView = this.baseController = this.dragHandler = null;
+};
+
+/**
+ * Check target element is expected condition for activate this plugins.
+ * @param {HTMLElement} target - The element to check
+ * @returns {string} - model id
+ */
+AlldayClick.prototype.checkExpectCondition = AlldayMove.prototype.checkExpectedCondition;
+
+/**
+ * Click event handler
+ * @param {object} clickEvent - click event data
+ * @emits AlldayClick#allday_click_click
+ */
+AlldayClick.prototype._onClick = function(clickEvent) {
+    var target = clickEvent.target,
+        timeView = this.checkExpectCondition(target),
+        blockElement = domutil.closest(target, '.schedule-view-allday-event-block'),
+        eventCollection = this.baseController.events;
+
+    if (!timeView || !blockElement) {
+        return;
+    }
+
+    eventCollection.doWhenHas(domutil.getData(blockElement, 'id'), function(model) {
+        /**
+         * @events AlldayClick#allday_click_click
+         * @type {object}
+         * @property {Event} model - model instance
+         */
+        this.fire('allday_click_click', {
+            model:  model
+        });
+    }, this);
+};
+
+util.CustomEvents.mixin(AlldayClick);
+
+module.exports = AlldayClick;
+
---
+++ test/app/handler/allday/click.spec.js
@@ -0,0 +1,46 @@
+var AlldayClick = window.ne.dooray.calendar.AlldayClick;
+describe('handler:AlldayClick', function() {
+    var mockInst,
+        mockCollection;
+    
+    beforeEach(function() {
+        // 컨트롤러에 id '2'인 객체가 있다고 가정
+        mockCollection = new ne.dooray.calendar.Collection();
+        mockCollection.add({_id: '2', text: 'hello'});
+
+        mockInst = jasmine.createSpyObj('AlldayClick', ['checkExpectCondition', 'fire']);
+        mockInst.baseController = {
+            events: mockCollection
+        };
+        mockInst.checkExpectCondition.and.returnValue(true);
+
+        spyOn(window.ne.dooray.calendar.domutil, 'closest').and.returnValue(true);
+    });
+
+    it('_onClick fire custom event "allday_click_click" when target element is related with one of event instance of base controllers.', function() {
+        // 클릭 대상 엘리먼트가 id '2'인 일정과 관계가 있을 때
+        spyOn(window.ne.dooray.calendar.domutil, 'getData').and.returnValue('2');
+
+        // 실행하면
+        AlldayClick.prototype._onClick.call(mockInst, {});
+
+        // 이벤트가 아래처럼 발생한다
+        expect(mockInst.fire).toHaveBeenCalledWith('allday_click_click', {
+            model: {
+                _id: '2',
+                text: 'hello'
+            }
+        });
+    });
+
+    it('AlldayClick doesn\'t fire custom event "allday_click_click" when no target or target is not related with events.', function() {
+        // 엘리먼트가 AlldayClick과 관계가 없다
+        mockInst.checkExpectCondition.and.returnValue(false);
+
+        // 실행하면
+        AlldayClick.prototype._onClick.call(mockInst, {});
+
+        // 무반응
+        expect(mockInst.fire).not.toHaveBeenCalled();
+    });
+});
