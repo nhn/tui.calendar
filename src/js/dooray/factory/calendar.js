5b94ea011600f42340d1653256e2e3fb0663bbdd
minhyeong.kim <minhyeong.kim@nhnent.com>
2015-10-29 11:00:31 +0900

    refactoring calendar factory module for dooray service

--- src/js/dooray/factory/calendar.js
+++ src/js/dooray/factory/calendar.js
@@ -6,15 +6,9 @@
 
 var util = global.ne.util;
 var datetime = require('../../common/datetime');
-
-// FACTORY
 var Calendar = require('../../factory/calendar');
-
-// CONTROLLER
 var DoorayBase = require('../controller/base');
 var Week = require('../../controller/viewMixin/week');
-
-// VIEW
 var serviceWeekViewFactory = require('./weekView');
 
 /**
@@ -62,12 +56,14 @@ function ServiceCalendar(options, container) {
         var controller = new DoorayBase(options),
             originFindByDateRange;
 
+        // 주뷰 컨트롤러 믹스인
         controller.Week = {};
         util.forEach(Week, function(method, methodName) {
             controller.Week[methodName] = util.bind(method, controller);
         });
 
-        // 마일스톤, 업무 뷰 용 뷰모델 처리기 추가
+        // 일정 조회 API에 기존 캘린더에 없었던 milstone, task를 지원하도록
+        // 하기 위해 메서드를 오버라이딩한다.
         originFindByDateRange = controller.Week.findByDateRange;
         controller.Week.findByDateRange = function(starts, ends) {
             var dateRange = util.map(datetime.range(
@@ -105,7 +101,8 @@ function ServiceCalendar(options, container) {
     })();
 
     // FullCalendar 기본 모듈은 category, dueDateClass 플래그를 모름. 때문에
-    // 이곳에서 이벤트 핸들러를 등록해서 category를 수동으로 지정해준다
+    // 이곳에서 이벤트 핸들러를 등록해서 일정 생성 전에 isAllDay플래그를 보고
+    // category를 수동으로 지정해준다
     controller.on('beforeCreateEvent', function(e) {
         var data = e.data;
 
@@ -123,13 +120,23 @@ function ServiceCalendar(options, container) {
 
 util.inherit(ServiceCalendar, Calendar);
 
-ServiceCalendar.prototype._onClick = function(e) {
-    this.fire('click', e);
+/**
+ * 각 뷰의 클릭 핸들러와 사용자 클릭 이벤트 핸들러를 잇기 위한 브릿지 개념의 이벤트 핸들러
+ * @emits ServiceCalendar#click
+ * @param {object} clickEventData - 'click' 핸들러의 이벤트 데이터
+ */
+ServiceCalendar.prototype._onClick = function(clickEventData) {
+    /**
+     * @events ServiceCalendar#click
+     * @type {object}
+     * @property {DoorayEvent} model - 클릭 이벤트 블록과 관련된 일정 모델 인스턴스
+     */
+    this.fire('click', clickEventData);
 };
 
 /**
  * 캘린더 팩토리 클래스와 주뷰, 월뷰의 이벤트 연결을 토글한다
- * @param {boolean} isAttah - true면 이벤트 연결함.
+ * @param {boolean} isAttach - true면 이벤트 연결함.
  * @param {Week|Month} view - 주뷰 또는 월뷰
  * @param {ServiceCalendar} calendar - 캘린더 팩토리 클래스
  */
@@ -147,7 +154,10 @@ ServiceCalendar.prototype._toggleViewEvent = function(isAttach, view, calendar) 
 };
 
 /**
+ * 주뷰, 월뷰 간 전환
  * @override
+ * @param {string} viewName - 'week', 'month' 중 하나
+ * @param {boolean} [force=false] - true 지정시 뷰 전환이 없어도 전환을 위한 동작을 수행한다
  */
 ServiceCalendar.prototype.toggleView = function(viewName, force) {
     var layout = this.layout,
@@ -162,21 +172,23 @@ ServiceCalendar.prototype.toggleView = function(viewName, force) {
     layout.childs.doWhenHas(viewName, function(view) {
         this._toggleViewEvent(false, view, this);
     }, this);
-
     layout.clear();
 
     if (viewName === 'week') {
         layout.addChild(function() {
             return serviceWeekViewFactory(controller, layout.container, dragHandler, options);
         });
+    } else if (viewName === 'month') {
+        //TODO: implements
+        console.log('월뷰 전환');
     }
 
-    this.currentViewName = viewName;
     layout.childs.doWhenHas(viewName, function(view) {
         this._toggleViewEvent(true, view, this);
     }, this);
-
     layout.render();
+
+    this.currentViewName = viewName;
 };
 
 module.exports = ServiceCalendar;
