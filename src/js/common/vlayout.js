8168cbc1d70b7fb17140afa8ddc8e96d47d656c8
minhyeong.kim <minhyeong.kim@nhnent.com>
2015-12-01 15:18:58 +0900

    Add vlayout, vpanel source

--- src/js/common/vlayout.js
+++ src/js/common/vlayout.js
@@ -3,8 +3,103 @@
  * @author NHN Ent. FE Development Team <dl_javascript@nhnent.com>
  */
 'use strict';
+var util = global.tui.util;
+var domutil = require('./domutil'),
+    View = require('../view/view'),
+    VPanel = require('./vpanel');
 
-function VLayout() {}
+/**
+ * @typedef PanelOptions
+ * @type {object}
+ * @property {number} [minHeight=0] - minimum height of panel
+ * @property {number} [maxHeight] - maximum height of panel. default is container height.
+ * @property {number} [height=0] - current panel height
+ * @property {boolean} [splitter=false] - is this panel uses splitter?
+ * @property {boolean} [autoHeight=false] - is this panel uses remain height of container?
+ * @property {string} [className=''] - className string for add created element
+ */
+
+/**
+ * @constructor
+ * @extends {View}
+ * @param {object} options - options for VLayout module
+ *  @param {PanelOptions[]} [options.panels] - panels to add layout when initialize
+ * @param {HTMLElement} container - container element
+ */
+function VLayout(options, container) {
+    var frag;
+
+    if (!(this instanceof VLayout)) {
+        return new VLayout(options, container);
+    }
+
+    View.call(this, container);
+    
+    /**
+     * @type {object}
+     */
+    this.options = util.extend({
+        panels: []
+    }, options);
+
+    /**
+     * @type {VPanel[]}
+     */
+    this._panels = [];
+
+    if (options.panels.length) {
+        frag = document.createDocumentFragment();
+
+        util.forEach(options.panels, function(panelOptions) {
+            this.addPanel(panelOptions, frag);
+        }, this);
+
+        this.container.appendChild(frag);
+    }
+
+    this.refresh();
+}
+
+util.inherit(VLayout, View);
+
+/**
+ * refresh each panels
+ */
+VLayout.prototype.refresh = function() {
+    var panelToFillHeight = [],
+        usedHeight = 0,
+        remainHeight;
+
+    util.forEach(this._panels, function(vPanel) {
+        var element = vPanel.container;
+
+        if (domutil.getData(element, 'autoHeight')) {
+            panelToFillHeight.push(vPanel);
+        } else {
+            usedHeight += vPanel.getHeight();
+        }
+    });
+
+    remainHeight = (this.getViewBound().height - usedHeight) / panelToFillHeight.length;
+
+    util.forEach(panelToFillHeight, function(vPanel) {
+        vPanel.setHeight(null, remainHeight);
+    });
+};
+
+/**
+ * add panel
+ * @param {PanelOptions} options - options for panel
+ * @param {container} container - container element
+ */
+VLayout.prototype.addPanel = function(options, container) {
+    var element = document.createElement('div'),
+        vPanel = new VPanel(options, element);
+
+    this._panels.push(vPanel);
+
+    container.appendChild(element);
+};
 
 module.exports = VLayout;
 
---
+++ src/js/common/vpanel.js
@@ -0,0 +1,77 @@
+/**
+ * @fileoverview Panel class for VLayout module
+ * @author NHN Ent. FE Development Team <dl_javascript@nhnent.com>
+ */
+'use strict';
+var util = global.tui.util;
+var config = require('../config'),
+    common = require('./common'),
+    domutil = require('./domutil'),
+    View = require('../view/view');
+
+/**
+ * @constructor
+ * @extends {View}
+ * @param {object} options - options for VPanel
+ * @param {HTMLElement} container - container element
+ */
+function VPanel(options, container) {
+    View.call(this, container);
+
+    this.options = util.extend({
+        minHeight: 0,
+        maxHeight: null,
+        height: null,
+        isSplitter: false,
+        autoHeight: false,
+        className: ''
+    }, options);
+
+    this._initPanel(options, container);
+}
+
+util.inherit(VPanel, View);
+
+/**
+ * set height of html element
+ * @param {HTMLElement} [container] - container element
+ * @param {number} newHeight - height
+ */
+VPanel.prototype.setHeight = function(container, newHeight) {
+    container = container || this.container;
+    container.style.height = newHeight + 'px';
+};
+
+VPanel.prototype.getHeight = function() {
+    return this.container.offsetHeight;
+};
+
+/**
+ * initialize panel element
+ * @param {PanelOptions} options - options for panel
+ * @param {HTMLDivElement} container - panel element
+ */
+VPanel.prototype._initPanel = function(options, container) {
+    var height;
+
+    if (options.isSplitter) {
+        domutil.addClass(container, config.classname('splitter'));
+        return;
+    }
+
+    domutil.addClass(container, options.className);
+
+    if (options.autoHeight) {
+        domutil.setData(container, 'autoHeight', true);
+    } else {
+        height = common.limit(options.height || 0, 
+            [options.minHeight],
+            options.maxHeight ? [options.maxHeight] : []
+        );
+
+        this.setHeight(container, height);
+    }
+};
+
+module.exports = VPanel;
+
