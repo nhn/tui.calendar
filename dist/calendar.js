74e3f126092701e39bbc2a32ee38dee773628ccf
minhyeong.kim <minhyeong.kim@nhnent.com>
2015-12-11 11:31:21 +0900

    Update 0.1.17 source

--- dist/calendar.js
+++ dist/calendar.js
@@ -2,7 +2,7 @@
 (function (global){
 /**
  * Dooray Calendar
- * @version 0.1.16
+ * @version 0.1.17
  */
 /* eslint vars-on-top:0, strict:0 */
 
@@ -7779,62 +7779,29 @@ module.exports = HandlebarsCompiler.template({"1":function(container,depth0,help
     + ((stack1 = helpers.each.call(depth0 != null ? depth0 : {},depth0,{"name":"each","hash":{},"fn":container.program(4, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
     + "</tr>\n";
 },"4":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<td class=\""
-    + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-"
-    + alias4(((helper = (helper = helpers.ymd || (depth0 != null ? depth0.ymd : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"ymd","hash":{},"data":data}) : helper)))
-    + " \n"
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.isNotThisMonth : depth0),{"name":"if","hash":{},"fn":container.program(5, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.selected : depth0),{"name":"if","hash":{},"fn":container.program(7, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.today : depth0),{"name":"if","hash":{},"fn":container.program(9, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.hasSchedule : depth0),{"name":"if","hash":{},"fn":container.program(11, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.weekend : depth0),{"name":"if","hash":{},"fn":container.program(13, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
+    + alias4(((helper = (helper = helpers.cssClass || (depth0 != null ? depth0.cssClass : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"cssClass","hash":{},"data":data}) : helper)))
     + "\">\n<button type=\"button\" tabindex=\"-1\" class=\""
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-date\">\n    <div class=\""
+    + "minicalendar-date\">\n<div class=\""
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
     + "minicalendar-label\"><span>"
-    + alias4(((helper = (helper = helpers.d || (depth0 != null ? depth0.d : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"d","hash":{},"data":data}) : helper)))
+    + alias4(((helper = (helper = helpers.date || (depth0 != null ? depth0.date : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"date","hash":{},"data":data}) : helper)))
     + "</span></div>\n</button>\n</td>\n";
-},"5":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return container.escapeExpression(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-other ";
-},"7":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return container.escapeExpression(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-focused ";
-},"9":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return container.escapeExpression(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-today ";
-},"11":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return container.escapeExpression(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-has-schedule ";
-},"13":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return container.escapeExpression(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-weekend ";
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var stack1, helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<table>\n<caption>\n    <button type=\"button\" class=\""
+  return "<table>\n<caption>\n<button type=\"button\" class=\""
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
     + "minicalendar-nav "
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
-    + "minicalendar-prev\"><span>&lt;</span></button>\n    <span class=\""
+    + "minicalendar-prev\"><span>&lt;</span></button>\n<span class=\""
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
     + "minicalendar-title\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</span>\n    <button type=\"button\" class=\""
+    + "</span>\n<button type=\"button\" class=\""
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
     + "minicalendar-nav "
     + alias4(((helper = (helper = helpers.CSS_PREFIX || (depth0 != null ? depth0.CSS_PREFIX : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"CSS_PREFIX","hash":{},"data":data}) : helper)))
@@ -7853,13 +7820,15 @@ module.exports = HandlebarsCompiler.template({"1":function(container,depth0,help
  */
 'use strict';
 
-var util = global.tui.util;
-var config = require('../../config');
-var View = require('../../view/view');
-var domutil = require('../../common/domutil');
-var domevent = require('../../common/domevent');
-var datetime = require('../../common/datetime');
-var tmpl = require('./minicalendar.hbs');
+var util = global.tui.util,
+    parseDateRx = /(\d{4}-\d{2}-\d{2})/;
+
+var config = require('../../config'),
+    View = require('../../view/view'),
+    domutil = require('../../common/domutil'),
+    domevent = require('../../common/domevent'),
+    datetime = require('../../common/datetime'),
+    tmpl = require('./minicalendar.hbs');
 
 /**
  * @constructor
@@ -7867,29 +7836,36 @@ var tmpl = require('./minicalendar.hbs');
  * @param {object} options - options for minicalendar view
  *  @param {number} [options.startDayOfWeek=0] - start day of week. default 0 (sunday)
  *  @param {string|Date} [options.renderMonth] - month to render
- *  @param {string[]} [options.highlightDate] - dates to highlight
- *  @param {string[]} [options.daynames] - array of each days name.
+ *  @param {string[]} [options.daynames] - array of each days name
+ *  @param {number[]} [options.weekendNumber] - number of weekend
+ *  @param {string} [options.selectedDate=''] - YYYY-MM-DD formatted selected date
  * @param {HTMLDivElement} container - element to use container
  */
 function MiniCalendar(options, container) {
-    var today = datetime.start(new Date());
+    var todayStart,
+        todayYMD;
 
     if (!(this instanceof MiniCalendar)) {
         return new MiniCalendar(options, container);
     }
 
     View.call(this, container);
+
     domutil.addClass(container, config.classname('minicalendar'));
     domevent.on(this.container, 'click', this._onClick, this);
+
+    todayStart = datetime.start(new Date());
+    todayYMD = datetime.format(todayStart, 'YYYY-MM-DD');
 
     /**
      * @type {object}
      */
     options = this.options = util.extend({
         startDayOfWeek: 0,
-        renderMonth: new Date(+today),
-        highlightDate: [],
-        daynames: ['일', '월', '화', '수', '목', '금', '토']
+        renderMonth: todayStart,
+        daynames: ['일', '월', '화', '수', '목', '금', '토'],
+        weekendNumber: [0, 6],
+        selectedDate: todayYMD
     }, options);
 
     // parse renderMonth options if it is an string
@@ -7902,9 +7878,14 @@ function MiniCalendar(options, container) {
      * @type {object}
      */
     this.hlData = {};
-    if (options.highlightDate.length) {
-        this.highlightDate(options.highlightDate);
-    }
+    this.hlData[todayYMD] = {
+        today: true
+    };
+
+    /**
+     * @type {Date}
+     */
+    this.selectedDate = new Date(+options.renderMonth);
 
     this.render();
 }
@@ -7941,43 +7922,36 @@ MiniCalendar.prototype._nav = function(buttonElement) {
 
 /**
  * Date button event handler
- * @fires Minicalendar#change
+ * @fires Minicalendar#click
  * @param {HTMLButtonElement} buttonElement - date button from _onClick event handler
  */
 MiniCalendar.prototype._date = function(buttonElement) {
     var td = domutil.closest(buttonElement, 'td'),
-        today = (new Date()),
-        previous,
-        selected,
-        eventData = {
-            before: this.getSelectedDate()
-        };
+        cssClass = domutil.getClass(td),
+        matches;
 
-    if (td) {
-        previous = domutil.find('.' + config.classname('minicalendar-focused'), this.container);
-
-        if (previous) {
-            domutil.removeClass(previous, config.classname('minicalendar-focused'));
-        }
-
-        domutil.addClass(td, config.classname('minicalendar-focused'));
-
-        selected = this.getSelectedDate();
-
-        if (datetime.isSameDate(selected, today)) {
-            domutil.addClass(td, config.classname('minicalendar-today'));
-        }
-
-        eventData.after = selected;
-
-        /**
-         * @event MiniCalendar#change
-         * @type {object}
-         * @property {Date} before - the date of before changed
-         * @property {Date} after - the date of after changed
-         */
-        this.fire('change', eventData);
+    if (!td) {
+        return;
     }
+
+    matches = cssClass.match(parseDateRx);
+
+    if (!matches) {
+        return;
+    }
+
+    this.selectedDate = datetime.parse(matches[0]);
+
+    this.render();
+
+    /**
+     * @event MiniCalendar#click
+     * @type {object}
+     * @property {Date} date - clicked date
+     */
+    this.fire('click', {
+        date: new Date(+this.selectedDate)
+    });
 };
 
 /**
@@ -8004,59 +7978,18 @@ MiniCalendar.prototype._onClick = function(clickEvent) {
 };
 
 /**
- * Get selected data
- * @returns {Date} selected date
- */
-MiniCalendar.prototype.getSelectedDate = function() {
-    var selected = domutil.find('.' + config.classname('minicalendar-focused'), this.container),
-        matches;
-
-    if (!selected) {
-        return null;
-    }
-
-    matches = selected.className.match(config.minicalendar.getDataRegExp);
-
-    if (!matches || matches.length < 2) {
-        return;
-    }
-
-    return datetime.parse(matches[1]);
-};
-
-/**
- * select specific date.
- * @param {Date|string} date - date to select
- */
-MiniCalendar.prototype.selectDate = function(date) {
-    var _date, td, button;
-
-    if (util.isString(date)) {
-        date = datetime.parse(date);
-    }
-
-    _date = datetime.format(date, 'YYYY-MM-DD');
-    td = domutil.find('.' + config.classname('minicalendar-') + _date, this.container);
-    button = domutil.find('button', td);
-
-    if (!td || !button) {
-        return;
-    }
-
-    this._date(button);
-};
-
-/**
  * Get minicalendar view model
  * @param {Date} renderDate - Date to render minicalendar
  * @param {number} startDayOfWeek - number of start of week (0:sun ...)
  * @returns {object} viewmodel
  */
 MiniCalendar.prototype._getViewModel = function(renderDate, startDayOfWeek) {
-    var daynames = this.options.daynames,
+    var opt = this.options,
+        daynames = opt.daynames,
+        weekendNumber = opt.weekendNumber,
         hlData = this.hlData,
-        today = datetime.start(new Date()),
-        isCurrentMonth = datetime.isSameMonth(renderDate, today),
+        selectedDate = this.selectedDate,
+        classPrefix = config.classname('minicalendar-'),
         viewModel = {
             title: datetime.format(renderDate, 'YYYY.MM'),
             startDayOfWeek: startDayOfWeek
@@ -8068,36 +8001,104 @@ MiniCalendar.prototype._getViewModel = function(renderDate, startDayOfWeek) {
     );
 
     viewModel.calendar = datetime.arr2dCalendar(renderDate, startDayOfWeek, function(date) {
-        var d = date.getDate(),
-            ymd = datetime.format(date, 'YYYY-MM-DD'),
+        var ymd = datetime.format(date, 'YYYY-MM-DD'),
             day = date.getDay(),
-            selected = false,
-            isToday = datetime.isSameDate(date, today),
-            dateIsInThisMonth = datetime.isSameMonth(date, renderDate);
+            cssClasses = util.keys(hlData[ymd] ? hlData[ymd] : {});
 
-        if (dateIsInThisMonth) {
-            if (isCurrentMonth) {
-                if (isToday) {
-                    selected = true;
-                    isToday = true;
-                }
-            } else if (d === 1) {
-                selected = true;
-            }
+        cssClasses.push(ymd);
+
+        if (datetime.isSameDate(date, selectedDate)) {
+            cssClasses.push('selected');
+        }
+
+        if (!datetime.isSameMonth(date, renderDate)) {
+            cssClasses.push('other-month');
+        }
+        
+        if (~util.inArray(day, weekendNumber)) {
+            cssClasses.push('weekend');
         }
 
         return {
-            d: d,
-            ymd: ymd,
-            hasSchedule: hlData[ymd],
-            isNotThisMonth: !dateIsInThisMonth,
-            weekend: (day === 0 || day === 6),
-            selected: selected,
-            today: isToday
+            date: date.getDate(),
+            cssClass: classPrefix + cssClasses.join(' ' + classPrefix)
         };
     });
 
     return viewModel;
+};
+
+/**
+ * Add highlight date
+ * @param {string} ymd - ymd
+ * @param {string} cssClass - cssClass
+ */
+MiniCalendar.prototype._addHlData = function(ymd, cssClass) {
+    var hlData = this.hlData[ymd];
+
+    if (!hlData) {
+        hlData = this.hlData[ymd] = {};
+    }
+
+    hlData[cssClass] = true;
+};
+
+/**
+ * Set highlight date range
+ * @param {Date} start - focus start date
+ * @param {Date} end - focus end date
+ * @param {string} cssClass - cssClass
+ */
+MiniCalendar.prototype._setHlDateRange = function(start, end, cssClass) {
+    var range = datetime.range(
+            datetime.start(start),
+            datetime.end(end),
+            datetime.MILLISECONDS_PER_DAY
+        );
+
+    util.forEach(range, function(date) {
+        this._addHlData(datetime.format(date, 'YYYY-MM-DD'), cssClass);
+    }, this);
+};
+
+/**
+ * Clear focus data
+ */
+MiniCalendar.prototype.clearFocusData = function() {
+    util.forEach(this.hlData, function(obj) {
+        delete obj['focused'];
+    });
+};
+
+/**
+ * Focus specific date range
+ * @param {Date} start - focus start date
+ * @param {Date} end - focus end date
+ */
+MiniCalendar.prototype.focusDateRange = function(start, end) {
+    this.clearFocusData();
+    this._setHlDateRange(start, end, 'focused');
+    this.render();
+};
+
+/**
+ * Get selected data
+ * @returns {Date} selected date
+ */
+MiniCalendar.prototype.getSelectedDate = function() {
+    return new Date(+this.selectedDate);
+};
+
+/**
+ * select specific date.
+ * @param {Date|string} date - date to select
+ */
+MiniCalendar.prototype.selectDate = function(date) {
+    date = util.isDate(date) ? date : datetime.parse(date);
+
+    this.selectedDate = new Date(+date);
+
+    this.render();
 };
 
 /**
@@ -8113,36 +8114,6 @@ MiniCalendar.prototype.render = function() {
     viewModel = this._getViewModel(renderDate, startDayOfWeek);
 
     container.innerHTML = tmpl(viewModel);
-};
-
-/**
- * Cache data for highlight specific dates in calendar.
- * @param {string[]} dateStrList - the array of dates to highlight. (YYYY-MM-DD)
- * @param {boolean} [silent=false] - set true for prevent auto rendering.
- */
-MiniCalendar.prototype.highlightDate = function(dateStrList, silent) {
-    var ownData = this.hlData;
-
-    util.forEach(dateStrList, function(ymd) {
-        ownData[ymd] = true;
-    });
-
-    if (!silent) {
-        this.render();
-    }
-};
-
-/**
- * Clear cached data for highlighting specific date for represent the date has schedule.
- * @param {boolean} [silent=false] - set true for prevent auto rendering.
- */
-MiniCalendar.prototype.clearHighlightDate = function(silent) {
-    delete this.hlData;
-    this.hlData = {};
-
-    if (!silent) {
-        this.render();
-    }
 };
 
 util.CustomEvents.mixin(MiniCalendar);
@@ -13208,7 +13179,9 @@ module.exports = {
 
     'time-tmpl': function(model) {
         return common.stripTags(model.title);
-    }
+    },
+
+    'minicalendar-tmpl': function(model) {}
 };
 
 
