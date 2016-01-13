4ac9d4224aab525aae9d130a86d1b023ee6e1b13
minhyeong.kim <minhyeong.kim@nhnent.com>
2016-01-13 14:31:02 +0900

    docs: weekdayIn*

--- src/js/view/week/weekdayInWeek.js
+++ src/js/view/week/weekdayInWeek.js
@@ -12,6 +12,7 @@ var Weekday = require('../weekday'),
 /**
  * @constructor
  * @extends {Weekday}
+ * @param {object} options - options for WeekdayInWeek view
  * @param {number} [options.containerHeight=40] - minimum height of event 
  *  container element.
  * @param {number} [options.containerButtonGutter=8] - free space at bottom to 
