d21b5a23d0e00ee98cf36141705d321c223c6068
minhyeong.kim <minhyeong.kim@nhnent.com>
2016-01-19 11:51:24 +0900

    feat: flaoting-layer
    add floating layer module
    
    close #114

---
+++ src/js/common/floatingLayer.js
@@ -0,0 +1,163 @@
+/**
+ * @fileoverview Floating layer module
+ * @author NHN Ent. FE Development Team <dl_javascript@nhnent.com>
+ */
+'use strict';
+var util = global.tui.util;
+var config = require('../config'),
+    domutil = require('../common/domutil'),
+    View = require('../view/view');
+
+/**
+ * @constructor
+ * @extends {View}
+ * @param {object} options - options for floating layer module
+ * @param {HTMLElement} container - parent continer for floating layer
+ */
+function FloatingLayer(options, container) {
+    var sibling = container[FloatingLayer.PROP_KEY],
+        layerContainer;
+
+    if (!sibling) {
+        sibling = container[FloatingLayer.PROP_KEY] = [];
+    }
+
+    sibling.push(this);
+
+    /**
+     * @type {Collection}
+     */
+    this.sibling = sibling;
+
+    /**
+     * @type {number}
+     */
+    this.zIndex = this.getLargestZIndex() || FloatingLayer.INIT_ZINDEX;
+
+    layerContainer = document.createElement('div');
+    layerContainer.style.display = 'none';
+    layerContainer.style.position = 'absolute';
+    domutil.addClass(layerContainer, config.classname('floating-layer'));
+    container.style.position = 'relative';
+    container.appendChild(layerContainer);
+
+    View.call(this, layerContainer);
+
+    /**
+     * @type {HTMLElement}
+     */
+    this.parent = container;
+}
+
+util.inherit(FloatingLayer, View);
+
+/**
+ * @const
+ */
+FloatingLayer.PROP_KEY = '__fe_floating_layer';
+
+/**
+ * @const
+ */
+FloatingLayer.INIT_ZINDEX = 999;
+
+/**
+ * Destroy floating layer instance. if there no instnace in parent container 
+ *
+ * remove instance cache property in container element
+ */
+FloatingLayer.prototype.destroy = function() {
+    var parent = this.parent,
+        sibling = this.sibling,
+        i = 0, cnt = sibling.length;
+
+    for (; i < cnt; i += 1) {
+        if (sibling[i] === this) {
+            sibling.splice(i, 1);
+            break;
+        }
+    }
+
+    if (!sibling.length) {
+        try {
+            delete parent[FloatingLayer.PROP_KEY];
+        } catch (e) {
+            parent[FloatingLayer.PROP_KEY] = null;
+        }
+
+        parent.style.position = '';
+    }
+
+    domutil.remove(this.container);
+
+    this.sibling = null;
+
+    View.prototype.destroy.call(this);
+};
+
+/**
+ * Set layer position
+ * @param {number} x - x coordinate of layer
+ * @param {number} y - y coordinate of layer
+ */
+FloatingLayer.prototype.setPosition = function(x, y) {
+    domutil.setPosition(this.container, x, y);
+};
+
+/**
+ * Set layer size
+ * @param {number} w - layer width
+ * @param {number} h - layer height
+ */
+FloatingLayer.prototype.setSize = function(w, h) {
+    var container = this.container;
+
+    container.style.width = w + 'px';
+    container.style.height = h + 'px';
+};
+
+/**
+ * Set layer content
+ * @param {string} html - html string
+ */
+FloatingLayer.prototype.setContent = function(html) {
+    this.container.innerHTML = html;
+};
+
+/**
+ * Get largest z-index from sibling layers
+ * @returns {number} largest z-index value
+ */
+FloatingLayer.prototype.getLargestZIndex = function() {
+    var zIndexes = util.map(this.sibling, function(layer) {
+        return layer.zIndex;
+    });
+
+    return Math.max.apply(null, zIndexes);
+};
+
+/**
+ * Set focus to layer
+ */
+FloatingLayer.prototype.focus = function() {
+    var zIndexForShow = this.getLargestZIndex() + 1;
+    this.container.style.zIndex = this.zIndex = zIndexForShow;
+};
+
+/**
+ * Show layer
+ */
+FloatingLayer.prototype.show = function() {
+    this.focus();
+    this.container.style.display = 'block';
+};
+
+/**
+ * Hide layer
+ */
+FloatingLayer.prototype.hide = function() {
+    this.container.style.display = 'none';
+};
+
+module.exports = FloatingLayer;
+
---
+++ test/app/common/floatingLayer.spec.js
@@ -0,0 +1,21 @@
+describe('FloatingLayer', function() {
+    var FloatingLayer = ne.dooray.calendar.FloatingLayer,
+        container;
+
+    beforeEach(function() {
+        container = document.createElement('div');
+    });
+
+    it('should remove cache property when no layer after destroying layer.', function() {
+        var f1 = new FloatingLayer({}, container),
+            f2 = new FloatingLayer({}, container);
+
+        f1.destroy();
+
+        expect(container[FloatingLayer.PROP_KEY].length).toBe(1);
+
+        f2.destroy();
+
+        expect(container[FloatingLayer.PROP_KEY]).toBeUndefined();
+    });
+});
