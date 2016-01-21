56ed20053dabe5332abd026a68791dc0ba06f7d7
minhyeong.kim <minhyeong.kim@nhnent.com>
2016-01-21 17:40:20 +0900

    refactor: month/creation

--- src/js/handler/month/creation.js
+++ src/js/handler/month/creation.js
@@ -3,7 +3,11 @@
  * @author NHN Ent. FE Development Team <dl_javascript@nhnent.com>
  */
 'use strict';
-var core = require('./core');
+var util = global.tui.util;
+
+var config = require('../../config'),
+    domutil = require('../../common/domutil'),
+    core = require('./core');
 
 /**
  * @constructor
@@ -30,7 +34,13 @@ function MonthCreation(dragHandler, monthView, baseController) {
     /**
      * @type {function}
      */
-    this.getBaseEventData = null;
+    this.getDate = null;
+
+    /**
+     * Cache for dragging session
+     * @type {object}
+     */
+    this._cache = null;
 
     dragHandler.on('dragStart', this._onDragStart, this);
 }
@@ -44,40 +54,87 @@ MonthCreation.prototype.destroy = function() {
 };
 
 /**
+ * Fire before create event
+ * @fires {MonthCreation#beforeCreateEvent}
+ * @param {object} eventCache - cache data from single dragging session
+ */
+MonthCreation.prototype._createEvent = function(eventCache) {
+    /**
+     * @event {MonthCreation#beforeCreateEvent}
+     * @type {object}
+     * @property {boolean} isAllDay - whether creating event is allday?
+     * @property {Date} starts - select start date
+     * @property {Date] ends - select end date
+     */
+    this.fire('beforeCreateEvent', {
+        isAllDay: true,
+        starts: eventCache.dragStartDate,
+        ends: eventCache.dragEndDate
+    });
+};
+
+/**
  * DragStart event handler
  * @param {object} dragStartEvent - dragStart event data
  */
 MonthCreation.prototype._onDragStart = function(dragStartEvent) {
-    var dragHandler = this.dragHandler,
-        originEvent = dragStartEvent.originEvent,
-        target = dragStartEvent.target;
+    var target = dragStartEvent.target;
 
-    // TODO: 빈 영역일때만 동작하도록 조건 설정.
+    if (!domutil.hasClass(target, config.classname('weekday-events'))) {
+        return;
+    }
 
-    dragHandler.on({
+    this.dragHandler.on({
         drag: this._onDrag,
         dragEnd: this._onDragEnd
     }, this);
 
-    this.getBaseEventData = core(dragHandler, this.monthView);
+    this.getDate = core(this.monthView);
+
+    this._cache = {
+        dragStartDate: this.getDate(dragStartEvent.originEvent)
+    };
 };
 
+/**
+ * Drag event handler
+ * @param {object} dragEvent - drag event data
+ */
 MonthCreation.prototype._onDrag = function(dragEvent) {
-    if (!this.getBaseEventData) {
+    var date;
+
+    if (!this.getDate) {
         return;
     }
 
-    this.getBaseEventData(dragEvent.originEvent);
+    date = this.getDate(dragEvent.originEvent);
+    //TODO: creation guide
 };
 
+/**
+ * DragEnd event handler
+ * @param {object} dragEndEvent - drag end event data
+ */
 MonthCreation.prototype._onDragEnd = function(dragEndEvent) {
     this.dragHandler.off({
         drag: this._onDrag,
         dragEnd: this._onDragEnd
     }, this);
 
-    this.getBaseEventData = null;
+    if (!this.getDate) {
+        return;
+    }
+
+    this._cache.dragEndDate = this.getDate(dragEndEvent.originEvent);
+
+    this._createEvent(this._cache);
+
+    this.getDateByMouseEvent = null;
+
+    this._cache = null;
 };
+
+util.CustomEvents.mixin(MonthCreation);
 
 module.exports = MonthCreation;
 
