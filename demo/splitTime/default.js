762540edbb32ee5f6a9293bc47d9abb93361bbfd
최민호/협업시스템개발팀/NE <minho-choi@nhnent.com>
2016-10-31 23:54:37 +0900

    켈린더 예제 페이지 수정

--- demo/splitTime/default.js
+++ demo/splitTime/default.js
@@ -1,80 +1,119 @@
 /* eslint no-console: 0, complexity: 0 */
 'use strict';
 (function(window, calendar) {
-    var cal, resizeThrottled,
-      idx = 5;
 
-    cal = calendar.SplitCalendar({
+    //* Only listed below formats avaliable.
+    //*
+    //* - YYYYMMDD
+    //* - YYYY/MM/DD
+    //* - YYYY-MM-DD
+    //* - YYYY/MM/DD HH:mm:SS
+    //* - YYYY-MM-DD HH:mm:SS
+    //TODO: 해당부분 수정 요망.
+    var a = new Date();
+    var currentDate = a.getFullYear() + '-' + (a.getMonth() + 1) + '-' + a.getDate() + ' ';
+
+    var calendarSet = {
+        '1': {
+            color: '#e8e8e8',
+            bgColor: '#585858',
+            render: false
+        },
+        '2': {
+            color: '#282828',
+            bgColor: '#dc9656',
+            render: false
+        },
+        '3': {
+            color: '#a16946',
+            bgColor: '#ab4642',
+            render: false
+        }
+      };
+
+    var cal1, cal2, cal3, cal4, cal5, cal6
+
+    cal1 = calendar.SplitCalendar({
         defaultView: 'day',
         template: {
             time: function(model) {
-                return model.title + ' <i class="fa fa-refresh"></i><br />' + model.starts;
+                console.log(model)
+                return model.title + model.origin.starts + '~' + model.origin.ends;
             }
         },
-        week: {
-            panelHeights: [80, 80, 120],
-            hourStart: 8,
-            hourEnd: 12,
-            renderStartDate: '2016-10-31T09:40:00+09:00',
-            renderEndDate: '2016-10-31T12:40:00+09:00'
-        }
-    }, document.getElementById('calendar'));
+        renderStartDate: currentDate + '11:00:00',
+        renderEndDate: currentDate + '12:00:00',
+        calendarColor: calendarSet
+    }, document.getElementById('calendar1'));
 
-    cal.setCalendarColor('1', {
-        color: '#e8e8e8',
-        bgColor: '#585858',
-        render: false
-    });
-    cal.setCalendarColor('2', {
-        color: '#282828',
-        bgColor: '#dc9656',
-        render: false
-    });
-    cal.setCalendarColor('3', {
-        color: '#a16946',
-        bgColor: '#ab4642',
-        render: false
-    });
-
-    cal.createEvents([{
+    cal1.createEvents([{
         id: '1',
         calendarID: '1',
         title: '스크럼',
         category: 'time',
         dueDateClass: '',
-        starts: '2016-10-31T09:40:00+09:00',
-        ends: '2016-10-31T10:40:00+09:00'
+        starts: currentDate + '09:40:00',
+        ends: currentDate + '10:40:00'
     }, {
         id: '2',
         calendarID: '2',
         title: '[홍길동]연차',
         category: 'time',
         dueDateClass: '',
-        starts: '2016-10-31T00:00:00+09:00',
-        ends: '2016-10-31T10:40:59+09:00'
+        starts: currentDate + '15:00:00+09:00',
+        ends: currentDate + '20:40:59+09:00'
     }, {
         id: '2123',
         calendarID: '3',
         title: '[3홍길동]연차',
         category: 'time',
         dueDateClass: '',
-        starts: '2016-10-31T16:00:00+09:00',
-        ends: '2016-10-31T16:59:59+09:00'
+        starts: currentDate + '16:00:00+09:00',
+        ends: currentDate + '16:59:59+09:00'
     }], true);
 
-    cal.render();
+    cal1.render();
 
-     //일정 클릭 이벤트 핸들러
-    cal.on({
-        'resizePanel': function(e) {
-            console.log('resizePanel', e);
-        }
-    });
-    //
-    //resizeThrottled = tui.util.throttle(function() {
-    //    cal.refresh();
-    //}, 50);
-    //
-    //window.addEventListener('resize', resizeThrottled);
+    cal2 = calendar.SplitCalendar({
+        defaultView: 'day',
+        template: {
+            time: function(model) {
+                return model.title + model.origin.starts + '~' + model.origin.ends;
+            }
+        },
+        renderStartDate: currentDate + '11:00:00',
+        renderEndDate: currentDate + '14:00:00',
+        calendarColor: calendarSet
+    }, document.getElementById('calendar2'));
+
+    cal2.createEvents([{
+        id: '1',
+        calendarID: '1',
+        title: '스크럼',
+        category: 'time',
+        dueDateClass: '',
+        starts: currentDate + '11:40:00+09:00',
+        ends: currentDate + '13:40:00+09:00'
+    }, {
+        id: '2',
+        calendarID: '2',
+        title: '[홍길동]연차',
+        category: 'time',
+        dueDateClass: '',
+        starts: currentDate + '09:00:00+09:00',
+        ends: currentDate + '15:40:59+09:00'
+    }, {
+        id: '2123',
+        calendarID: '3',
+        title: '[3홍길동]연차',
+        category: 'time',
+        dueDateClass: '',
+        starts: currentDate + '10:00:00+09:00',
+        ends: currentDate + '20:59:59+09:00'
+    }], true);
+
+    cal2.render();
+
+
     window.cal = cal;
 })(window, ne.dooray.calendar);
