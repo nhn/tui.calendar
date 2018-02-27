b8762baad75fa985fb1a6611a2163e141c855b6c
Dongsik Yoo <dongsik.yoo@nhnent.com>
2018-02-27 16:07:38 +0900

    chore: IE Demo(use rgba() as calendar color)

--- examples/js/data/calendars.js
+++ examples/js/data/calendars.js
@@ -30,6 +30,17 @@ function findCalendar(id) {
     return found;
 }
 
+function hexToRGBA(hex) {
+    var radix = 16;
+    var r = parseInt(hex.slice(1, 3), radix),
+        g = parseInt(hex.slice(3, 5), radix),
+        b = parseInt(hex.slice(5, 7), radix),
+        a = parseInt(hex.slice(7, 9), radix) / 255 || 1;
+    var rgba = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + a + ')';
+
+    return rgba;
+}
+
 (function() {
     var calendar;
     var id = 0;
@@ -38,40 +49,40 @@ function findCalendar(id) {
     id += 1;
     calendar.id = String(id);
     calendar.name = 'My calendar';
-    calendar.color = '#79c87e';
-    calendar.textColor = '#2c9032';
-    calendar.bgColor = '#79c87e33';
-    calendar.borderColor = '#79c87e';
+    calendar.color = hexToRGBA('#79c87e');
+    calendar.textColor = hexToRGBA('#2c9032');
+    calendar.bgColor = hexToRGBA('#79c87e33');
+    calendar.borderColor = hexToRGBA('#79c87e');
     addCalendar(calendar);
 
     calendar = new CalendarInfo();
     id += 1;
     calendar.id = String(id);
     calendar.name = 'Company';
-    calendar.color = '#4091d0';
-    calendar.textColor = '#2b6796';
-    calendar.bgColor = '#4091d033';
-    calendar.borderColor = '#4091d0';
+    calendar.color = hexToRGBA('#4091d0');
+    calendar.textColor = hexToRGBA('#2b6796');
+    calendar.bgColor = hexToRGBA('#4091d033');
+    calendar.borderColor = hexToRGBA('#4091d0');
     addCalendar(calendar);
 
     calendar = new CalendarInfo();
     id += 1;
     calendar.id = String(id);
     calendar.name = 'Family';
-    calendar.color = '#da1b1b';
-    calendar.textColor = '#9a1313';
-    calendar.bgColor = '#da1b1b33';
-    calendar.borderColor = '#da1b1b';
+    calendar.color = hexToRGBA('#da1b1b');
+    calendar.textColor = hexToRGBA('#9a1313');
+    calendar.bgColor = hexToRGBA('#da1b1b33');
+    calendar.borderColor = hexToRGBA('#da1b1b');
     addCalendar(calendar);
 
     calendar = new CalendarInfo();
     id += 1;
     calendar.id = String(id);
     calendar.name = 'Travel';
-    calendar.color = '#57c6b4';
-    calendar.textColor = '#1a907d';
-    calendar.bgColor = '#57c6b433';
-    calendar.borderColor = '#57c6b4';
+    calendar.color = hexToRGBA('#57c6b4');
+    calendar.textColor = hexToRGBA('#1a907d');
+    calendar.bgColor = hexToRGBA('#57c6b433');
+    calendar.borderColor = hexToRGBA('#57c6b4');
     addCalendar(calendar);
 
     calendar = new CalendarInfo();
