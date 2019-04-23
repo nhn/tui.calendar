ac587a179017fc03a07c8e4aa18ab430c8263494
Jenny Cho <jenny.cho@nhn.com>
2019-04-23 12:21:53 +0900

    fix #Dooray-QA/9540

--- src/js/view/month/more.js
+++ src/js/view/month/more.js
@@ -145,8 +145,13 @@ More.prototype.render = function(viewModel) {
     var styles = this._getStyles(this.theme);
     var maxVisibleSchedulesInLayer = 10;
     var height = '';
-    var isLastRow = weekItem.parentElement.lastElementChild === weekItem;
-    var isLastCol = target.parentElement.lastElementChild === target;
+    // var isLastRow = weekItem.parentElement.lastElementChild === weekItem;
+    // var isLastCol = target.parentElement.lastElementChild === target;
+    var containerSize = domutil.getSize(this.container);
+    var calWidth = 0;
+    var calHeight = 0;
+    var isOverWidth = false;
+    var isOverHeight = false;
 
     this._viewModel = util.extend(viewModel, {
         scheduleGutter: opt.scheduleGutter,
@@ -181,17 +186,22 @@ More.prototype.render = function(viewModel) {
 
     layer.setContent(tmpl(viewModel));
 
-    if (isLastRow && isLastCol) {
+    calWidth = Number(pos[0].split('%')[0]) * containerSize[0] / 100;
+    calHeight = Number(pos[1].split('%')[0]) * containerSize[1] / 100;
+    isOverWidth = calWidth + width >= containerSize[0];
+    isOverHeight = calHeight + height >= containerSize[1];
+
+    if (isOverWidth && isOverHeight) {
         layer.setLTRB({
             right: 0,
             bottom: 0
         });
-    } else if (isLastRow && !isLastCol) {
+    } else if (!isOverWidth && isOverHeight) {
         layer.setLTRB({
             left: pos[0],
             bottom: 0
         });
-    } else if (!isLastRow && isLastCol) {
+    } else if (isOverWidth && !isOverHeight) {
         layer.setLTRB({
             right: 0,
             top: pos[1]
