e802db342ee25ce9fd0f9c7476e0464156a961a2
Jenny Cho <jenny.cho@nhn.com>
2019-09-30 20:09:48 +0900

    fix: If the user directly inputs the end time as 00:00, render the schedule correctly.

--- src/js/handler/time/creationGuide.js
+++ src/js/handler/time/creationGuide.js
@@ -128,7 +128,7 @@ TimeCreationGuide.prototype._getUnitData = function(relatedView) {
         viewHeight = relatedView.getViewBound().height,
         hourLength = viewOpt.hourEnd - viewOpt.hourStart,
         todayStart = datetime.parse(viewOpt.ymd),
-        todayEnd = datetime.end(todayStart);
+        todayEnd = datetime.getNextStartDay(todayStart);
 
     todayStart.setHours(0, 0, 0, 0);
     todayStart.setHours(viewOpt.hourStart);
