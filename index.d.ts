24afb60bbc619cdafe11da3671285f6fe22ab45f
Jenny Cho <jenny.cho@nhn.com>
2020-01-20 11:56:05 +0900

    chore: version up 1.12.5-dooray-sp98-200120

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for TOAST UI Calendar v1.12.5-dooray-sp96-191205
+// Type definitions for TOAST UI Calendar v1.12.5-dooray-sp98-200120
 // TypeScript Version: 3.2.1
 
 declare namespace tuiCalendar {
