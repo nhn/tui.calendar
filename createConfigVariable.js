4815e1817129dd3c960ec0e9e365a35952c1af45
조정은 <jenny.cho@nhn.com>
2020-04-17 11:12:14 +0900

    chore: fix eslint (apply PR feedbacks)

--- createConfigVariable.js
+++ createConfigVariable.js
@@ -1,35 +1,36 @@
-const fs = require("fs");
-const path = require("path");
-const config = require(path.resolve(process.cwd(), "tuidoc.config.json"));
+const {readdirSync, writeFileSync} = require('fs');
+const {resolve} = require('path');
+const config = require(resolve(process.cwd(), 'tuidoc.config.json'));
 const examples = config.examples || {};
-const { filePath, globalErrorLogVariable } = examples;
+const {filePath, globalErrorLogVariable} = examples;
 
 /**
  * Get Examples Url
  */
 function getTestUrls() {
-    if (!filePath) {
-        throw Error("not exist examples path at tuidoc.config.json");
+  if (!filePath) {
+    throw Error('not exist examples path at tuidoc.config.json');
+  }
+
+  const urlPrefix = 'http://nhn.github.io/tui.image-editor/latest';
+
+  const testUrls = readdirSync(filePath).reduce((urls, fileName) => {
+    if (/html$/.test(fileName)) {
+      urls.push(`${urlPrefix}/${filePath}/${fileName}`);
     }
 
-    const urlPrefix = "http://nhn.github.io/tui.image-editor/latest";
+    return urls;
+  }, []);
 
-    const testUrls = fs.readdirSync(filePath).reduce((urls, fileName) => {
-        if (/html$/.test(fileName)) {
-            urls.push(`${urlPrefix}/${filePath}/${fileName}`);
-        }
-        return urls;
-    }, []);
-
-    fs.writeFileSync("url.txt", testUrls.join(", "));
+  writeFileSync('url.txt', testUrls.join(', '));
 }
 
 function getGlobalVariable() {
-    if (!globalErrorLogVariable) {
-        throw Error("not exist examples path at tuidoc.config.json");
-    }
+  if (!globalErrorLogVariable) {
+    throw Error('not exist examples path at tuidoc.config.json');
+  }
 
-    fs.writeFileSync("errorVariable.txt", globalErrorLogVariable);
+  writeFileSync('errorVariable.txt', globalErrorLogVariable);
 }
 
 getTestUrls();
