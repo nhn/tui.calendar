f17deb2f75c59768244f883e5d1b54a1666f7fa4
Jenny Cho <jenny.cho@nhn.com>
2020-04-17 11:34:33 +0900

    chore: apply eslint rule for es5

--- createConfigVariable.js
+++ createConfigVariable.js
@@ -1,36 +1,44 @@
-const {readdirSync, writeFileSync} = require('fs');
-const {resolve} = require('path');
-const config = require(resolve(process.cwd(), 'tuidoc.config.json'));
-const examples = config.examples || {};
-const {filePath, globalErrorLogVariable} = examples;
+/* eslint-disable no-sync */
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var config = require(path.resolve(process.cwd(), 'tuidoc.config.json'));
+var examples = config.examples || {};
+var filePath = examples.filePath;
+var globalErrorLogVariable = examples.globalErrorLogVariable;
 
 /**
  * Get Examples Url
  */
 function getTestUrls() {
-  if (!filePath) {
-    throw Error('not exist examples path at tuidoc.config.json');
-  }
+    var urlPrefix = 'http://nhn.github.io/tui.calendar/latest';
+    var testUrls;
 
-  const urlPrefix = 'http://nhn.github.io/tui.image-editor/latest';
-
-  const testUrls = readdirSync(filePath).reduce((urls, fileName) => {
-    if (/html$/.test(fileName)) {
-      urls.push(`${urlPrefix}/${filePath}/${fileName}`);
+    if (!filePath) {
+        throw Error('not exist examples path at tuidoc.config.json');
     }
 
-    return urls;
-  }, []);
+    testUrls = fs.readdirSync(filePath).reduce(function(urls, fileName) {
+        if (/html$/.test(fileName)) {
+            urls.push(urlPrefix + '/' + filePath + '/' + fileName);
+        }
 
-  writeFileSync('url.txt', testUrls.join(', '));
+        return urls;
+    }, []);
+
+    fs.writeFileSync('url.txt', testUrls.join(', '));
 }
 
+/**
+ * Get Global Variable
+ */
 function getGlobalVariable() {
-  if (!globalErrorLogVariable) {
-    throw Error('not exist examples path at tuidoc.config.json');
-  }
+    if (!globalErrorLogVariable) {
+        throw Error('not exist examples path at tuidoc.config.json');
+    }
 
-  writeFileSync('errorVariable.txt', globalErrorLogVariable);
+    fs.writeFileSync('errorVariable.txt', globalErrorLogVariable);
 }
 
 getTestUrls();
