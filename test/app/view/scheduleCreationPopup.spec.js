5f3f3b84da70a275ba32fd3d7fcd0b2b685171e4
조정은 <jenny.cho@nhn.com>
2020-04-23 18:15:58 +0900

    improvement : improved default creation popup location(improvement #576) (#577)
    * improvement : improved default creation popup location(improvement #576)
    
    * test: fix test code for creation popup layer placement
    
    * chore: apply test convention
    
    * chore: fix jsdoc indent

--- test/app/view/scheduleCreationPopup.spec.js
+++ test/app/view/scheduleCreationPopup.spec.js
@@ -4,53 +4,46 @@ var ScheduleCreationpPopup = require('../../../src/js/view/popup/scheduleCreatio
 
 /* eslint-disable object-property-newline */
 describe('ScheduleCreationpPopup#_calcRenderingData', function() {
-    var popupSize, windowSize;
+    var popupSize, containerSize;
 
     beforeEach(function() {
         popupSize = {width: 200, height: 80};
-        windowSize = {top: 0, left: 0, bottom: 300, right: 500, width: 500, height: 300};
+        containerSize = {top: 50, left: 100, bottom: 300, right: 500, width: 500, height: 300};
     });
 
-    it('should set position to top, center of guideElements', function() {
-        var guideBound = {top: 100, left: 100, bottom: 200, right: 400};
-        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, windowSize, guideBound);
+    it('it is usually placed at the top center of the guide element', function() {
+        var guideBound = {top: 200, left: 100, bottom: 200, right: 400};
+        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, containerSize, guideBound);
 
-        expect(posData.x).toBe(150);
-        expect(posData.y).toBe(23);
+        expect(posData.x).toBe(50);
+        expect(posData.y).toBe(67);
         expect(posData.arrow.direction).toBe('arrow-bottom');
         expect(posData.arrow.position).toBeUndefined();
     });
 
-    it('should set position to bottom center of guideElements, ', function() {
-        var guideBound = {top: 75, left: 100, bottom: 120, right: 400};
-        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, windowSize, guideBound);
+    it('if it overflows the top of the container, it should be placed under the guide element', function() {
+        var guideBound = {top: 100, left: 100, bottom: 200, right: 400};
+        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, containerSize, guideBound);
 
-        expect(posData.x).toBe(150);
-        expect(posData.y).toBe(129);
+        expect(posData.x).toBe(50);
+        expect(posData.y).toBe(153);
         expect(posData.arrow.direction).toBe('arrow-top');
         expect(posData.arrow.position).toBeUndefined();
     });
 
-    it('should shift to left, if popup is overflowed', function() {
-        var guideBound = {top: 100, left: 400, bottom: 200, right: 500};
-        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, windowSize, guideBound);
-
-        expect(posData.x).toBe(300);
-        expect(posData.y).toBe(23);
-        expect(posData.arrow.direction).toBe('arrow-bottom');
-        expect(posData.arrow.position).toBe(142);
-    });
-
-    it('should set position left 0 of screen, if popup is overflowed both left and right', function() {
-        var guideBound = {top: 100, left: 100, bottom: 200, right: 400};
-        var posData;
-
-        popupSize = {width: 600, height: 80};
-        posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, windowSize, guideBound);
+    it('when overflowing to the left of the container, the left value is set to 0 and the left value of the arrow is also set.', function() {
+        var guideBound = {top: 200, left: 50, bottom: 200, right: 200};
+        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, containerSize, guideBound);
 
         expect(posData.x).toBe(0);
-        expect(posData.y).toBe(23);
-        expect(posData.arrow.direction).toBe('arrow-bottom');
-        expect(posData.arrow.position).toBe(242);
+        expect(posData.arrow.position).toBeDefined();
+    });
+
+    it('when it overflows to the right of the container, the popup is aligned to the right and the left value of the arrow should be set', function() {
+        var guideBound = {top: 200, left: 400, bottom: 200, right: 500};
+        var posData = ScheduleCreationpPopup.prototype._calcRenderingData(popupSize, containerSize, guideBound);
+
+        expect(posData.x).toBe(200);
+        expect(posData.arrow.position).toBeDefined();
     });
 });
