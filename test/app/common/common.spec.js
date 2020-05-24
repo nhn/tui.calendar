b322670387b89d870a23c02d60661429f1abc695
Дилян Палаузов <git-dpa@aegee.org>
2020-05-24 07:56:24 +0000

    Remove unused common.pick2

--- test/app/common/common.spec.js
+++ test/app/common/common.spec.js
@@ -27,39 +27,6 @@ describe('module:common', function() {
         expect(to.a).toBeUndefined();
     });
 
-    describe('pick2()', function() {
-        it('pick2 can use return value by method chaining.', function() {
-            var obj = {
-                one: {
-                    two: 'hello',
-                    method: function() {return 'good';},
-                    test: jasmine.createSpy('common')
-                }
-            };
-
-            expect(common.pick2(obj, 'one', 'two').val()).toBe('hello');
-            expect(common.pick2(obj, 'one', 'two').then(function(val) { return val + ' world'; })).toBe('hello world');
-            expect(common.pick2(obj, 'one', 'two').then(function() { return this + ' world'; })).toBe('hello world');
-            expect(common.pick2(obj, 'one').then('method')).toBe('good');
-            expect(common.pick2(obj, 'def').then('good')).toBeUndefined();
-
-            common.pick2(obj, 'one').then('test', 'number');
-            expect(obj.one.test).toHaveBeenCalledWith('number');
-        });
-
-        it('then() is not invoke when supplid path\'s parameter is not exist.', function() {
-            var obj = {
-                one: {}
-            };
-
-            var spy = jasmine.createSpy('pick2:then');
-
-            common.pick2(obj, 'two').then(spy);
-
-            expect(spy).not.toHaveBeenCalled();
-        });
-    });
-
     it('limit can applying limitation supplied number', function() {
         expect(common.limit(3, [0], [2])).toBe(2);
         expect(common.limit(-20, [0], [2])).toBe(0);
