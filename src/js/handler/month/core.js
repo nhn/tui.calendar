f499fc8c38a51249b58684d0d098687823f99e26
Jenny Cho <jenny.cho@nhn.com>
2020-07-01 10:35:50 +0900

    fix: when resize schedule, fix reverted schedule on monthly view (#646 issue 1)

--- src/js/handler/month/core.js
+++ src/js/handler/month/core.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 /**
  * @fileoverview Module for calculate date by month view and mouse event object
  * @author NHN FE Development Lab <dl_javascript@nhn.com>
@@ -8,6 +9,7 @@ var util = require('tui-code-snippet');
 var common = require('../../common/common'),
     domutil = require('../../common/domutil'),
     domevent = require('../../common/domevent');
+var datetime = require('../../common/datetime');
 var mfloor = Math.floor;
 
 /**
@@ -91,7 +93,7 @@ function getMousePosDate(monthView) {
             y: y,
             sizeX: dayCount,
             sizeY: weekCount,
-            date: date,
+            date: datetime.end(date),
             weekdayView: weekdayView,
             triggerEvent: mouseEvent.type
         };
