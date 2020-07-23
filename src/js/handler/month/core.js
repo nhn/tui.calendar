b875b97ce03fc888a0169c74682bf020023018fe
Jenny Cho <jenny.cho@nhn.com>
2020-07-23 17:24:02 +0900

    fix: add datetime import

--- src/js/handler/month/core.js
+++ src/js/handler/month/core.js
@@ -8,7 +8,8 @@
 var util = require('tui-code-snippet');
 var common = require('../../common/common'),
     domutil = require('../../common/domutil'),
-    domevent = require('../../common/domevent');
+    domevent = require('../../common/domevent'),
+    datetime = require('../../common/datetime');
 var mfloor = Math.floor;
 
 /**
