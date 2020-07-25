a34efacd563149200a165fa2c79b7f68cfe876bb
Дилян Палаузов <git-dpa@aegee.org>
2020-07-25 06:09:54 +0000

    domevent.mousevent: all supported browsers have document.createEvent

--- src/js/common/domevent.js
+++ src/js/common/domevent.js
@@ -314,7 +314,7 @@ var domevent = {
      *
      * Tested at
      *
-     * - IE7 ~ IE11
+     * - IE9 ~ IE11
      * - Chrome
      * - Firefox
      * - Safari
@@ -323,14 +323,10 @@ var domevent = {
      * @returns {MouseEvent} Virtual mouse event.
      */
     mouseEvent: function(type, eventObj) {
-        var evt,
-            e;
-
-        e = util.extend({
+        var e = util.extend({
             bubbles: true,
             cancelable: (type !== 'mousemove'),
             view: window,
-            wheelDelta: 0,
             detail: 0,
             screenX: 0,
             screenY: 0,
@@ -340,28 +336,15 @@ var domevent = {
             altKey: false,
             shiftKey: false,
             metaKey: false,
-            button: 0,
-            relatedTarget: undefined  // eslint-disable-line
+            button: 0
         }, eventObj);
-
-        if (typeof document.createEvent === 'function') {
-            evt = document.createEvent('MouseEvents');
-            evt.initMouseEvent(type,
-                e.bubbles, e.cancelable, e.view, e.detail,
-                e.screenX, e.screenY, e.clientX, e.clientY,
-                e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
-                e.button, document.body.parentNode
-            );
-        } else if (document.createEventObject) {
-            evt = document.createEventObject();
-
-            util.forEach(e, function(value, propName) {
-                evt[propName] = value;
-            }, this);
-            evt.button = {0: 1,
-                1: 4,
-                2: 2}[evt.button] || evt.button;
-        }
+        var evt = document.createEvent('MouseEvents');
+        evt.initMouseEvent(type,
+            e.bubbles, e.cancelable, e.view, e.detail,
+            e.screenX, e.screenY, e.clientX, e.clientY,
+            e.ctrlKey, e.altKey, e.shiftKey, e.metaKey,
+            e.button, document.body.parentNode
+        );
 
         return evt;
     },
