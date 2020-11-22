bb234eb513f9caf6a1c56c0b747e6d68364c3484
Jenny Cho <jenny.cho@nhn.com>
2020-11-22 22:33:38 +1030

    test: add initUtil TC

---
+++ test/app/common/intlUtil.spec.js
@@ -0,0 +1,27 @@
+/* eslint-disable space-before-function-paren */
+'use strict';
+
+var util = require('tui-code-snippet');
+var intlUtil = require('common/intlUtil');
+
+describe('common/intlUtil', function () {
+    it('supportIntl()', function () {
+        var supported = !util.browser.msie;
+
+        expect(intlUtil.supportIntl('Asia/Seoul')).toBe(supported);
+    });
+
+    it('offsetCalculator()', function () {
+        var d1 = new Date(2020, 10, 22, 0, 0, 0);
+        var d2 = new Date(2020, 5, 22, 0, 0, 0);
+
+        if (intlUtil.supportIntl('Asia/Seoul')) {
+            expect(intlUtil.offsetCalculator('Asia/Seoul', d1.getTime())).toBe(-540);
+        }
+
+        if (intlUtil.supportIntl('America/New_York')) {
+            expect(intlUtil.offsetCalculator('America/New_York', d1.getTime())).toBe(300);
+            expect(intlUtil.offsetCalculator('America/New_York', d2.getTime())).toBe(240);
+        }
+    });
+});
