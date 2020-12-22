2993ef629cab3feace4989a467b45af2519294b8
Jenny Cho <jenny.cho@nhn.com>
2020-12-22 21:50:35 +0900

    test: change polyfill

--- test/app/common/intlUtil.spec.js
+++ test/app/common/intlUtil.spec.js
@@ -5,10 +5,12 @@ require('es6-set/implement');
 require('weakmap-polyfill');
 require('@formatjs/intl-getcanonicallocales/polyfill');
 require('@formatjs/intl-locale/polyfill');
+require('@formatjs/intl-pluralrules/polyfill');
+require('@formatjs/intl-pluralrules/locale-data/en');
 require('@formatjs/intl-numberformat/polyfill');
-require('@formatjs/intl-numberformat/locale-data/en');
+// require('@formatjs/intl-numberformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/polyfill');
-require('@formatjs/intl-datetimeformat/locale-data/en');
+// require('@formatjs/intl-datetimeformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/add-all-tz');
 
 var intlUtil = require('common/intlUtil');
@@ -22,13 +24,8 @@ describe('common/intlUtil', function() {
         var d1 = new Date(2020, 10, 22, 0, 0, 0);
         var d2 = new Date(2020, 5, 22, 0, 0, 0);
 
-        if (intlUtil.supportIntl('Asia/Seoul')) {
-            expect(intlUtil.offsetCalculator('Asia/Seoul', d1.getTime())).toBe(-540);
-        }
-
-        if (intlUtil.supportIntl('America/New_York')) {
-            expect(intlUtil.offsetCalculator('America/New_York', d1.getTime())).toBe(300);
-            expect(intlUtil.offsetCalculator('America/New_York', d2.getTime())).toBe(240);
-        }
+        expect(intlUtil.offsetCalculator('Asia/Seoul', d1.getTime())).toBe(-540);
+        expect(intlUtil.offsetCalculator('America/New_York', d1.getTime())).toBe(300);
+        expect(intlUtil.offsetCalculator('America/New_York', d2.getTime())).toBe(240);
     });
 });
