268b4521a937767f4d994aa55624540276584623
Jenny Cho <jenny.cho@nhn.com>
2020-12-22 21:35:21 +0900

    test: add numberformat local data

--- test/app/common/intlUtil.spec.js
+++ test/app/common/intlUtil.spec.js
@@ -6,6 +6,7 @@ require('weakmap-polyfill');
 require('@formatjs/intl-getcanonicallocales/polyfill');
 require('@formatjs/intl-locale/polyfill');
 require('@formatjs/intl-numberformat/polyfill');
+require('@formatjs/intl-numberformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/polyfill');
 require('@formatjs/intl-datetimeformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/add-all-tz');
