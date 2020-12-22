18939e2320a95be1d4d15eab250e6ebf217ede17
Jenny Cho <jenny.cho@nhn.com>
2020-12-22 21:53:53 +0900

    test: add locale data polyfill

--- test/app/common/intlUtil.spec.js
+++ test/app/common/intlUtil.spec.js
@@ -8,9 +8,9 @@ require('@formatjs/intl-locale/polyfill');
 require('@formatjs/intl-pluralrules/polyfill');
 require('@formatjs/intl-pluralrules/locale-data/en');
 require('@formatjs/intl-numberformat/polyfill');
-// require('@formatjs/intl-numberformat/locale-data/en');
+require('@formatjs/intl-numberformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/polyfill');
-// require('@formatjs/intl-datetimeformat/locale-data/en');
+require('@formatjs/intl-datetimeformat/locale-data/en');
 require('@formatjs/intl-datetimeformat/add-all-tz');
 
 var intlUtil = require('common/intlUtil');
