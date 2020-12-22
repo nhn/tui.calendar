f8c56ae112d4367deb690335d12a637944a040b2
Jenny Cho <jenny.cho@nhn.com>
2020-12-22 22:24:58 +0900

    test: change intl polyfill

--- test/app/common/intlUtil.spec.js
+++ test/app/common/intlUtil.spec.js
@@ -1,17 +1,6 @@
 /* eslint-disable vars-on-top */
 'use strict';
 
-require('es6-set/implement');
-require('weakmap-polyfill');
-require('@formatjs/intl-getcanonicallocales/polyfill');
-require('@formatjs/intl-locale/polyfill');
-require('@formatjs/intl-pluralrules/polyfill');
-require('@formatjs/intl-numberformat/polyfill');
-require('@formatjs/intl-numberformat/locale-data/en');
-require('@formatjs/intl-datetimeformat/polyfill');
-require('@formatjs/intl-datetimeformat/locale-data/en');
-require('@formatjs/intl-datetimeformat/add-all-tz');
-
 var intlUtil = require('common/intlUtil');
 
 describe('common/intlUtil', function() {
--- test/index.js
+++ test/index.js
@@ -1,4 +1,16 @@
 /*eslint-disable*/
+
+require('es6-set/implement');
+require('weakmap-polyfill');
+require('@formatjs/intl-getcanonicallocales/polyfill');
+require('@formatjs/intl-locale/polyfill');
+require('@formatjs/intl-pluralrules/polyfill');
+require('@formatjs/intl-numberformat/polyfill');
+require('@formatjs/intl-numberformat/locale-data/en');
+require('@formatjs/intl-datetimeformat/polyfill');
+require('@formatjs/intl-datetimeformat/locale-data/en');
+require('@formatjs/intl-datetimeformat/add-all-tz');
+
 require('view/template/helper');
 fixture.setBase('test/fixtures');
 var testsContext = require.context('.', true, /spec.js$/);
