7b38a8f4cbc82059d54d239527719843cb105818
Dũng Nguyễn <qngnud@gmail.com>
2021-02-25 23:43:36 +0700

    revert test

--- src/js/view/template/helper.js
+++ src/js/view/template/helper.js
@@ -508,7 +508,7 @@ function msToTime(milliseconds) {
     hours = (hours < 10) ? '0' + hours : hours;
     minutes = (minutes < 10) ? '0' + minutes : minutes;
 
-    return 'hah';
+    return hours + ':' + minutes;
 }
 
 /**
