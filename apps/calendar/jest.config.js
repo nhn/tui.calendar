f58f1a8826edd818b01f9ead6135afdb73f2621f
Jenny Cho <jenny.cho@nhn.com>
2021-03-29 21:18:45 +0900

    chore: remove unnecessary config or command script

--- apps/calendar/jest.config.js
+++ apps/calendar/jest.config.js
@@ -7,14 +7,10 @@ module.exports = {
     '^@test/(.*)$': '<rootDir>/test/$1',
     '\\.(css)$': '<rootDir>/__mocks__/styleMock.ts',
   },
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
   globals: { 'ts-jest': { babelConfig: true } },
   watchPathIgnorePatterns: ['<rootDir>/.storybook', '<rootDir>/.stories', '/node_modules/'],
   setupFilesAfterEnv: [
     '<rootDir>/test/matcher/matrices.ts',
     '<rootDir>/test/matcher/viewModels.ts',
   ],
-  testMatch: ['**/test/**/*.spec.ts'],
 };
