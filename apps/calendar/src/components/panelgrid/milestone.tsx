d04e615f87cdbf93d48f399be401660edbd9f8f1
lja1018 <jaeeon.lim@nhn.com>
2021-05-10 10:51:50 +0900

    chore: change panel type

--- apps/calendar/src/components/panelgrid/milestone.tsx
+++ apps/calendar/src/components/panelgrid/milestone.tsx
@@ -36,12 +36,13 @@ export const Milestone: FunctionComponent<Props> = ({
   panelHeight = DEFAULT_MILESTONE_PANEL_HEIGHT,
 }) => {
   const columnWidth = timesWidth * timezonesCount;
+  const panelType = 'milestone';
   const { state, dispatch } = useContext(PanelStore);
 
   useEffect(() => {
     dispatch({
       type: PanelActionType.UPDATE_PANEL_HEIGHT,
-      panelType: 'milestone',
+      panelType,
       state: {
         panelHeight,
       },
@@ -50,17 +51,17 @@ export const Milestone: FunctionComponent<Props> = ({
 
   return (
     <Fragment>
-      <PanelTitle width={columnWidth} template="milestoneTitle" model="milestone" />
+      <PanelTitle width={columnWidth} template={panelType} model={panelType} />
       <div className={cls('panel-milestone')}>
         <PanelGrid
-          name="milestone"
+          name={panelType}
           gridInfoList={gridInfoList}
           events={events}
           defaultPanelHeight={panelHeight}
           options={state?.milestone}
         />
         <PanelEvents
-          name="milestone"
+          name={panelType}
           gridInfoList={gridInfoList}
           events={events}
           options={state?.milestone}
