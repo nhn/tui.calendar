95bd7890b0eade21e871e3009509316f682fa718
lja1018 <jaeeon.lim@nhn.com>
2021-05-13 16:02:58 +0900

    feat: add basic dayView component

---
+++ apps/calendar/src/components/view/day.tsx
@@ -0,0 +1,7 @@
+import { FunctionComponent, h } from 'preact';
+
+const Day: FunctionComponent = () => {
+  return <h1>Day View</h1>;
+};
+
+export default Day;
---
+++ apps/calendar/stories/dayView.stories.tsx
@@ -0,0 +1,44 @@
+import { h } from 'preact';
+import { Story } from '@storybook/preact';
+
+import { ProviderWrapper } from '@stories/util/providerWrapper';
+import DayView from '@src/components/view/dayView';
+
+export default { title: 'DayView' };
+
+const Template: Story = (args) => (
+  <ProviderWrapper options={args.options}>
+    <DayView />
+  </ProviderWrapper>
+);
+
+export const basic = Template.bind({});
+
+export const narrowWeekend = Template.bind({});
+narrowWeekend.args = {
+  options: { month: { narrowWeekend: true } },
+};
+
+export const startDayOfWeek = Template.bind({});
+startDayOfWeek.args = {
+  options: { month: { startDayOfWeek: 3 } },
+};
+
+export const dayNames = Template.bind({});
+dayNames.args = {
+  options: {
+    month: {
+      daynames: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+    },
+  },
+};
+
+export const workweek = Template.bind({});
+workweek.args = {
+  options: { month: { workweek: true } },
+};
+
+export const twoWeeks = Template.bind({});
+twoWeeks.args = {
+  options: { month: { visibleWeeksCount: 2 } },
+};
