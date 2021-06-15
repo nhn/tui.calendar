06b6561d5c214f053eea614e5edcd62769e57c49
lja1018 <jaeeon.lim@nhn.com>
2021-06-15 18:48:29 +0900

    chore: move panelContainer hooks

---
+++ apps/calendar/src/components/hooks/panelContainer.ts
@@ -0,0 +1,41 @@
+import { Ref, useCallback, useEffect, useState } from 'preact/hooks';
+
+import { cls } from '@src/util/cssHelper';
+
+// @TODO: merge 'usePanel' hooks after apply layout component to month component
+export function usePanelContainer(containerRef: Ref<HTMLDivElement>, selector: string) {
+  const [panel, setPanel] = useState<HTMLElement | null>(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      const panelContainer: HTMLElement | null = containerRef.current.querySelector(
+        `${cls('.panel')}${selector}`
+      );
+
+      if (panelContainer) {
+        setPanel(panelContainer);
+      }
+    }
+  }, [containerRef, selector]);
+
+  return panel;
+}
+
+export function usePanel(selector: string) {
+  const [panel, setPanel] = useState<HTMLElement | null>(null);
+
+  const containerRefCallback = useCallback(
+    (ref: HTMLElement) => {
+      if (ref) {
+        const panelContainer: HTMLElement | null = ref.querySelector(`${cls('.panel')}${selector}`);
+
+        if (panelContainer) {
+          setPanel(panelContainer);
+        }
+      }
+    },
+    [selector]
+  );
+
+  return { panel, containerRefCallback };
+}
