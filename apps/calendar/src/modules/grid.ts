5d172e54e3f2f2c10a26a67504a4c8734e53a5df
Dohyung Ahn <dohyung.ahn@nhn.com>
2021-06-17 15:36:56 +0900

    refactor: remove unused action `updateLayoutHeight`

--- apps/calendar/src/modules/grid.ts
+++ apps/calendar/src/modules/grid.ts
@@ -16,9 +16,6 @@ export const grid = {
   name: 'grid',
   state: initialLayout,
   actions: {
-    updateLayoutHeight(state: LayoutState, { height }: { height: number }) {
-      return { ...state, layout: { ...state.layout, height } };
-    },
     updatePanelHeight(state: LayoutState, { type, height }: { type: string; height: number }) {
       return {
         ...state,
