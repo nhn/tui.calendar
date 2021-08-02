dccb7ea7f3a624b642bf4b29e5fc6915212a7f15
lja1018 <jaeeon.lim@nhn.com>
2021-08-02 11:18:24 +0900

    fix: add `ProviderWrapper` to layout stories

--- apps/calendar/stories/layout.stories.tsx
+++ apps/calendar/stories/layout.stories.tsx
@@ -4,31 +4,35 @@ import { Layout } from '@src/components/layout';
 import Panel from '@src/components/panel';
 import { Direction, ResizeMode } from '@src/controller/layout';
 
+import { ProviderWrapper } from '@stories/util/providerWrapper';
+
 export default { title: 'Layout' };
 
 export const vertical = () => (
-  <Layout height={500}>
-    <Panel name="DayName" height={50}>
-      <div style="border-bottom: 1px solid #bbb; height: 100%;">DayName Panel</div>
-    </Panel>
-    <Panel name="Milestone" resizable height={50}>
-      <div>Milestone Panel</div>
-    </Panel>
-    <Panel name="Task" resizable>
-      <div>Task Panel</div>
-    </Panel>
-    <Panel name="All Day" resizable minHeight={30}>
-      <div>All Day Panel</div>
-    </Panel>
-    <Panel name="Time" autoSize={1}>
-      <div style="height: 100%; border-bottom: 1px solid #bbb">Time Panel</div>
-    </Panel>
-    <span>dk</span>
-  </Layout>
+  <ProviderWrapper>
+    <Layout height={500}>
+      <Panel name="DayName" height={50}>
+        <div style="border-bottom: 1px solid #bbb; height: 100%;">DayName Panel</div>
+      </Panel>
+      <Panel name="Milestone" resizable height={50}>
+        <div>Milestone Panel</div>
+      </Panel>
+      <Panel name="Task" resizable>
+        <div>Task Panel</div>
+      </Panel>
+      <Panel name="All Day" resizable minHeight={30}>
+        <div>All Day Panel</div>
+      </Panel>
+      <Panel name="Time" autoSize={1}>
+        <div style="height: 100%; border-bottom: 1px solid #bbb">Time Panel</div>
+      </Panel>
+      <span>dk</span>
+    </Layout>
+  </ProviderWrapper>
 );
 
-export const horizontal = () => {
-  return (
+export const horizontal = () => (
+  <ProviderWrapper>
     <Layout direction={Direction.ROW} height={300}>
       <Panel name="Mon" resizable>
         <div>Mon</div>
@@ -52,32 +56,38 @@ export const horizontal = () => {
         <div>Sun</div>
       </Panel>
     </Layout>
-  );
-};
+  </ProviderWrapper>
+);
 
 export const verticalWithOverflow = () => (
-  <Layout>
-    <Panel name="DayName" height={50} overflowY>
-      <div style="border-bottom: 1px solid #bbb; height: 300px;">Overflow-Y Panel</div>
-    </Panel>
-  </Layout>
+  <ProviderWrapper>
+    <Layout>
+      <Panel name="DayName" height={50} overflowY>
+        <div style="border-bottom: 1px solid #bbb; height: 300px;">Overflow-Y Panel</div>
+      </Panel>
+    </Layout>
+  </ProviderWrapper>
 );
 
 export const horizontalWithOverflow = () => (
-  <Layout direction={Direction.ROW}>
-    <Panel name="DayName" width={200} height={200} overflowX>
-      <div style="border-right: 1px solid #bbb; width: 300px;">Overflow-X Panel</div>
-    </Panel>
-  </Layout>
+  <ProviderWrapper>
+    <Layout direction={Direction.ROW}>
+      <Panel name="DayName" width={200} height={200} overflowX>
+        <div style="border-right: 1px solid #bbb; width: 300px;">Overflow-X Panel</div>
+      </Panel>
+    </Layout>
+  </ProviderWrapper>
 );
 
 export const resizeModeAbsolute = () => (
-  <Layout resizeMode={ResizeMode.ABSOLUTE}>
-    <Panel name="1" resizable resizerHeight={10}>
-      <div style="height: 200px;">Panel 1</div>
-    </Panel>
-    <Panel name="2" resizable>
-      <div style="height: 200px;">Panel 2</div>
-    </Panel>
-  </Layout>
+  <ProviderWrapper>
+    <Layout resizeMode={ResizeMode.ABSOLUTE}>
+      <Panel name="1" resizable resizerHeight={10}>
+        <div style="height: 200px;">Panel 1</div>
+      </Panel>
+      <Panel name="2" resizable>
+        <div style="height: 200px;">Panel 2</div>
+      </Panel>
+    </Layout>
+  </ProviderWrapper>
 );
