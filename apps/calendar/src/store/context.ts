d1fb870c8b32b786ba947fe728631dcd4eae812b
Dohyung Ahn <dohyung.ahn@nhn.com>
2021-08-18 10:25:38 +0900

    refactor: merge `useStore` hook and `createStoreContext`
    * in order to inject store to components and
      use the store in the `Calendar` instance same time,
      the store should be injected through the Provider component.
    * `useStore` hook doesn't have to be separated.

--- apps/calendar/src/store/context.ts
+++ apps/calendar/src/store/context.ts
@@ -1,38 +1,115 @@
 import { createContext, createElement, FunctionComponent } from 'preact';
-import { useContext, useMemo, useRef } from 'preact/hooks';
+import { useContext, useEffect, useLayoutEffect, useMemo, useReducer, useRef } from 'preact/hooks';
 
-import { EqualityChecker, StateSelector, StateWithActions, StoreHooks, UseStore } from '@t/store';
+import { isNil, isUndefined } from '@src/util/utils';
 
-export function createStoreContext<State extends StateWithActions>(
-  storeCreator: () => StoreHooks<State>
-) {
-  const StoreContext = createContext<StoreHooks<State> | null>(null);
-  const storeHooks = storeCreator();
+import { EqualityChecker, InternalStoreAPI, StateSelector, StateWithActions } from '@t/store';
 
-  const Provider: FunctionComponent = ({ children }) => {
-    const storeRef = useRef<StoreHooks<State>>();
+/**
+ * Inspired by Zustand
+ *
+ * See more: https://github.com/pmndrs/zustand
+ */
 
-    if (!storeRef.current) {
-      storeRef.current = storeHooks;
-    }
+const isSSR = isUndefined(window) || !window.navigator;
 
-    return createElement(StoreContext.Provider, { value: storeRef.current, children });
+const useIsomorphicLayoutEffect = isSSR ? useEffect : useLayoutEffect;
+
+export function createStoreContext<State extends StateWithActions>() {
+  const StoreContext = createContext<InternalStoreAPI<State> | null>(null);
+
+  const StoreProvider: FunctionComponent<{ store: InternalStoreAPI<State> }> = ({
+    children,
+    store,
+  }) => {
+    return createElement(StoreContext.Provider, { value: store, children });
   };
 
-  const useStore: UseStore<State> = <StateSlice>(
-    selector?: StateSelector<State, StateSlice>,
-    equalityFn: EqualityChecker<State> = Object.is
+  const useStore = <StateSlice>(
+    selector: StateSelector<State, StateSlice>,
+    equalityFn: EqualityChecker<StateSlice> = Object.is
   ) => {
     const storeCtx = useContext(StoreContext);
 
-    if (!storeCtx?.useStore) {
+    if (isNil(storeCtx)) {
       throw new Error('StoreProvider is not found');
     }
 
-    // we cannot call `storeCtx.useStore` in condition because it violates the rules of hook.
-    // `selector` type should be `StateSelector<State, StateSlice>`
-    // but TS cannot infer the type of `selector` clearly.
-    return storeCtx.useStore(selector as StateSelector<State, State>, equalityFn);
+    // a little trick to invoke re-render to notify hook consumers(usually components)
+    const [, notify] = useReducer((notifyCount) => notifyCount + 1, 0) as [never, () => void];
+
+    const state = storeCtx.getState();
+    const stateRef = useRef(state);
+    const selectorRef = useRef(selector);
+    const equalityFnRef = useRef(equalityFn);
+    const hasErrorRef = useRef(false);
+    // `null` can be a valid state slice.
+    const currentSliceRef = useRef<StateSlice | undefined>();
+
+    if (isUndefined(currentSliceRef.current)) {
+      currentSliceRef.current = selector(state);
+    }
+
+    let newStateSlice: StateSlice | undefined;
+    let hasNewStateSlice = false;
+
+    const shouldGetNewSlice =
+      stateRef.current !== state ||
+      selectorRef.current !== selector ||
+      equalityFnRef.current !== equalityFn ||
+      hasErrorRef.current;
+    if (shouldGetNewSlice) {
+      newStateSlice = selector(state);
+      hasNewStateSlice = !equalityFn(currentSliceRef.current, newStateSlice);
+    }
+
+    useIsomorphicLayoutEffect(() => {
+      if (hasNewStateSlice) {
+        currentSliceRef.current = newStateSlice as StateSlice;
+      }
+
+      stateRef.current = state;
+      selectorRef.current = selector;
+      equalityFnRef.current = equalityFn;
+      hasErrorRef.current = false;
+    });
+
+    // NOTE: There is edge case that state is changed before subscription
+    const stateBeforeSubscriptionRef = useRef(state);
+    useIsomorphicLayoutEffect(() => {
+      const listener = () => {
+        try {
+          const nextState = storeCtx.getState();
+          const nextStateSlice = selectorRef.current(nextState);
+
+          const shouldUpdateState = !equalityFnRef.current(
+            currentSliceRef.current as StateSlice,
+            nextStateSlice
+          );
+          if (shouldUpdateState) {
+            stateRef.current = nextState;
+            currentSliceRef.current = newStateSlice;
+
+            notify();
+          }
+        } catch (e) {
+          // This will be rarely happened, unless we don't pass the arguments to actions properly.
+          // eslint-disable-next-line no-console
+          console.error('[toastui-calendar] failed to update state', e.message);
+          hasErrorRef.current = true;
+          notify();
+        }
+      };
+
+      const unsubscribe = storeCtx.subscribe(listener);
+      if (storeCtx.getState() !== stateBeforeSubscriptionRef.current) {
+        listener();
+      }
+
+      return unsubscribe;
+    }, []);
+
+    return hasNewStateSlice ? (newStateSlice as StateSlice) : currentSliceRef.current;
   };
 
   /**
@@ -42,17 +119,16 @@ export function createStoreContext<State extends StateWithActions>(
   const useInternalStore = () => {
     const storeCtx = useContext(StoreContext);
 
-    if (!storeCtx) {
+    if (isNil(storeCtx)) {
       throw new Error('StoreProvider is not found');
     }
 
-    return useMemo(() => storeCtx.internalStore, [storeCtx]);
+    return useMemo(() => storeCtx, [storeCtx]);
   };
 
   return {
-    Provider,
+    StoreProvider,
     useStore,
     useInternalStore,
-    internalStore: storeHooks.internalStore,
   };
 }
--- apps/calendar/test/app/store/context.spec.tsx
+++ apps/calendar/test/app/store/context.spec.tsx
@@ -2,108 +2,272 @@ import { h } from 'preact';
 import { useEffect, useState } from 'preact/hooks';
 
 import { createStoreContext } from '@src/store/context';
-import { createStoreHook } from '@src/store/hook';
+import { createStore } from '@src/store/internal';
 
-import { fireEvent, render, screen, waitFor } from '@testing-library/preact';
+import { InternalStoreAPI } from '@t/store';
+import { fireEvent, render, screen } from '@testing-library/preact';
 
-describe('createStoreContext', () => {
-  type CounterStore = {
-    count: number;
-    incCount: () => void;
-    setCount: (value: number) => void;
-  };
-  const storeCreator = () =>
-    createStoreHook<CounterStore>((set) => ({
-      count: 0,
-      incCount: () => set((state) => ({ ...state, count: state.count + 1 })),
-      setCount: (value: number) => set((state) => ({ ...state, count: value })),
-    }));
-  const {
-    Provider: StoreProvider,
-    useStore,
-    useInternalStore,
-    internalStore,
-  } = createStoreContext<CounterStore>(storeCreator);
-  const Counter = () => {
-    const count = useStore((state) => state.count);
-
-    return <div>Current count is: {count}</div>;
-  };
-  const CounterButtons = () => {
-    const [incCount, setCount] = useStore((state) => [state.incCount, state.setCount]);
-
-    return (
-      <div>
-        <button onClick={incCount}>+</button>
-        <button onClick={() => setCount(0)}>reset</button>
-      </div>
-    );
-  };
-
-  beforeEach(() => {
-    internalStore.reset();
-  });
-
-  it('should inject store to components with Provider', () => {
-    const { container } = render(
-      <StoreProvider>
-        <Counter />
-        <CounterButtons />
-      </StoreProvider>
-    );
-
-    expect(container).toHaveTextContent(/current count is: 0/i);
-
-    fireEvent.click(screen.getByText('+'));
-    expect(container).toHaveTextContent(/current count is: 1/i);
-
-    fireEvent.click(screen.getByText('reset'));
-    expect(container).toHaveTextContent(/current count is: 0/i);
-  });
-
-  it('should access store internal by `useStoreInternal`', () => {
-    const CounterMultiplier = () => {
-      const storeInternal = useInternalStore();
-      const [multipliedCounter, setMultipliedCounter] = useState(0);
-
-      useEffect(
-        () =>
-          storeInternal.subscribe(
-            (newCounter: number) => setMultipliedCounter(newCounter * 2),
-            (state) => state.count
-          ),
-        [storeInternal]
-      );
-
-      return <div>x2 count: {multipliedCounter}</div>;
+describe('New Store', () => {
+  describe('Simple State', () => {
+    type CounterStore = {
+      count: number;
+      increment: () => void;
+      setCount: (n: number) => void;
     };
 
-    const { container } = render(
-      <StoreProvider>
-        <CounterMultiplier />
-        <CounterButtons />
-      </StoreProvider>
-    );
+    let store: InternalStoreAPI<CounterStore>;
+    const { StoreProvider, useStore, useInternalStore } = createStoreContext<CounterStore>();
 
-    expect(container).toHaveTextContent(/x2 count: 0/i);
+    const Counter = () => {
+      const count = useStore((state) => state.count);
 
-    fireEvent.click(screen.getByText('+'));
-    expect(container).toHaveTextContent(/x2 count: 2/i);
+      return <div>Current count is: {count}</div>;
+    };
+    const CounterButtons = () => {
+      const [incCount, setCount] = useStore((state) => [state.increment, state.setCount]);
+
+      return (
+        <div>
+          <button onClick={incCount}>+</button>
+          <button onClick={() => setCount(0)}>reset</button>
+        </div>
+      );
+    };
+
+    beforeEach(() => {
+      store = createStore((set) => ({
+        count: 0,
+        increment: () =>
+          set(({ count }) => ({
+            count: count + 1,
+          })),
+        setCount: (n: number) => set(() => ({ count: n })),
+      }));
+    });
+
+    it('should inject store to components with Provider', () => {
+      const { container } = render(
+        <StoreProvider store={store}>
+          <Counter />
+          <CounterButtons />
+        </StoreProvider>
+      );
+
+      expect(container).toHaveTextContent(/current count is: 0/i);
+
+      fireEvent.click(screen.getByText('+'));
+      expect(container).toHaveTextContent(/current count is: 1/i);
+
+      fireEvent.click(screen.getByText('reset'));
+      expect(container).toHaveTextContent(/current count is: 0/i);
+    });
+
+    it('should access store internal by `useStoreInternal`', () => {
+      const CounterMultiplier = () => {
+        const storeInternal = useInternalStore();
+        const [multipliedCounter, setMultipliedCounter] = useState(0);
+
+        useEffect(
+          () =>
+            storeInternal.subscribe(
+              (newCounter: number) => setMultipliedCounter(newCounter * 2),
+              (state) => state.count
+            ),
+          [storeInternal]
+        );
+
+        return <div>x2 count: {multipliedCounter}</div>;
+      };
+
+      const { container } = render(
+        <StoreProvider store={store}>
+          <CounterMultiplier />
+          <CounterButtons />
+        </StoreProvider>
+      );
+
+      expect(container).toHaveTextContent(/x2 count: 0/i);
+
+      fireEvent.click(screen.getByText('+'));
+      expect(container).toHaveTextContent(/x2 count: 2/i);
+    });
   });
 
-  it('can use the internal store which affects components when the state changed', async () => {
-    const { container } = render(
-      <StoreProvider>
-        <Counter />
-      </StoreProvider>
-    );
-    internalStore.setState((state) => ({ count: state.count + 1 }));
+  describe('Complex state', () => {
+    type Store = {
+      todo: {
+        todos: { id: string; title: string; isDone: boolean }[];
+        filter: 'all' | 'active' | 'done';
+      };
+      search: {
+        todoSearchKeyword: string;
+      };
+      dispatch: {
+        todo: {
+          addTodo: (title: string) => void;
+          changeTodoFilter: (filter: 'all' | 'active' | 'done') => void;
+        };
+        search: {
+          changeTodoSearchKeyword: (keyword: string) => void;
+        };
+      };
+    };
 
-    // NOTE: The timing of view update is a little bit different when directly call `setState` through the internal
-    // store.
-    // Reference: https://github.com/pmndrs/zustand#calling-actions-outside-a-react-event-handler
-    // However, Preact doesn't provide the API such as `unstable_batchedUpdates`.
-    // so we just wait for the component to be updated.
-    await waitFor(() => expect(container).toHaveTextContent(/current count is: 1/i));
+    let store: InternalStoreAPI<Store>;
+    const { StoreProvider, useStore } = createStoreContext<Store>();
+
+    const SearchInput = () => {
+      const searchKeyword = useStore((state) => state.search.todoSearchKeyword);
+      const handleChange = useStore((state) => state.dispatch.search.changeTodoSearchKeyword);
+
+      return (
+        <div>
+          <span>Current search keyword: {searchKeyword}</span>
+          <label htmlFor="search-keyword">
+            Search:
+            <input
+              id="search-keyword"
+              type="text"
+              value={searchKeyword}
+              onChange={(e) => handleChange(e.currentTarget.value ?? '')}
+            />
+          </label>
+        </div>
+      );
+    };
+
+    const Todos = () => {
+      const todos = useStore((state) => state.todo.todos);
+
+      return (
+        <ul role="list">
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.title}</li>
+          ))}
+        </ul>
+      );
+    };
+
+    beforeEach(() => {
+      const todoNames = ['a', 'b', 'c', 'd'];
+      store = createStore<Store>((set) => ({
+        todo: {
+          todos: todoNames.map((name) => ({
+            title: name,
+            id: `id-${name}`,
+            isDone: name === 'd',
+          })),
+          filter: 'all',
+        },
+        search: {
+          todoSearchKeyword: '',
+        },
+        dispatch: {
+          todo: {
+            addTodo: (title) =>
+              set(({ todo }) => ({
+                todo: {
+                  ...todo,
+                  todos: todo.todos.concat({ title, id: `id-${title}`, isDone: false }),
+                },
+              })),
+            changeTodoFilter: (filter) =>
+              set(({ todo }) => ({
+                todo: {
+                  ...todo,
+                  filter,
+                },
+              })),
+          },
+          search: {
+            changeTodoSearchKeyword: (keyword) =>
+              set(({ search }) => ({
+                search: {
+                  ...search,
+                  todoSearchKeyword: keyword,
+                },
+              })),
+          },
+        },
+      }));
+    });
+
+    it('should not affect other states when modifying a certain scope of state', () => {
+      render(
+        <StoreProvider store={store}>
+          <SearchInput />
+          <Todos />
+        </StoreProvider>
+      );
+
+      const input = screen.getByLabelText('Search:');
+      const todolist = screen.getByRole('list');
+
+      expect(todolist.children).toHaveLength(4);
+
+      fireEvent.change(input, { target: { value: 'b' } });
+
+      const searchResult = screen.getByText(/current search keyword:\sb/i);
+      expect(searchResult).toBeInTheDocument();
+      expect(todolist.children).toHaveLength(4);
+    });
+
+    it('should be able to create derived states with selectors', () => {
+      const FilteredTodo = () => {
+        const filteredTodos = useStore((state) => {
+          const { todos, filter } = state.todo;
+          const { todoSearchKeyword } = state.search;
+          const filterBySearchKeyword = todoSearchKeyword
+            ? (todo: Store['todo']['todos'][number]) => todo.title.startsWith(todoSearchKeyword)
+            : Boolean;
+
+          if (filter === 'all') {
+            return todos.filter(filterBySearchKeyword);
+          }
+
+          return todos
+            .filter((todo) => (filter === 'done' ? todo.isDone : !todo.isDone))
+            .filter(filterBySearchKeyword);
+        });
+
+        return (
+          <ul role="list">
+            {filteredTodos.map((todo) => (
+              <li key={todo.id}>{todo.title}</li>
+            ))}
+          </ul>
+        );
+      };
+      const TodoFilter = () => {
+        const changeFilter = useStore((state) => state.dispatch.todo.changeTodoFilter);
+
+        return (
+          <div>
+            <button onClick={() => changeFilter('all')}>All</button>
+            <button onClick={() => changeFilter('active')}>Active</button>
+            <button onClick={() => changeFilter('done')}>Done</button>
+          </div>
+        );
+      };
+
+      render(
+        <StoreProvider store={store}>
+          <FilteredTodo />
+          <TodoFilter />
+        </StoreProvider>
+      );
+
+      const getTodoList = () => screen.getByRole('list');
+      const changeFilter = (filter: 'All' | 'Active' | 'Done') =>
+        fireEvent.click(screen.getByText(filter));
+
+      expect(getTodoList().children).toHaveLength(4);
+
+      changeFilter('Active');
+      expect(getTodoList().children).toHaveLength(3);
+
+      changeFilter('Done');
+      expect(getTodoList().children).toHaveLength(1);
+    });
   });
 });
