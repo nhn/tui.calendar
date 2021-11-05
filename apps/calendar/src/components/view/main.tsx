ac4bedc7f29c073ee904007300be028b235c98d0
Dohyung Ahn <dohyung.ahn@nhn.com>
2021-11-05 14:57:43 +0900

    feat: create new main view

--- apps/calendar/src/components/view/main.tsx
+++ apps/calendar/src/components/view/main.tsx
@@ -1,7 +1,24 @@
 import { FunctionComponent, h } from 'preact';
+import { useMemo } from 'preact/hooks';
 
-const Main: FunctionComponent = () => {
-  return <div></div>;
+import DayView from '@src/components/view/dayView';
+import MonthView from '@src/components/view/monthView';
+import WeekView from '@src/components/view/weekView';
+import { useStore } from '@src/contexts/calendarStore';
+import { ViewType } from '@src/model';
+import { topLevelStateSelector } from '@src/selectors';
+
+const views: {
+  [k in ViewType]: FunctionComponent;
+} = {
+  month: MonthView,
+  week: WeekView,
+  day: DayView,
 };
 
-export default Main;
+export const Main: FunctionComponent = () => {
+  const { currentView } = useStore(topLevelStateSelector('view'));
+  const CurrentViewComponent = useMemo(() => views[currentView] || null, [currentView]);
+
+  return <CurrentViewComponent />;
+};
---
+++ apps/calendar/stories/main.stories.tsx
@@ -0,0 +1,53 @@
+import { FunctionComponent, h } from 'preact';
+
+import { Main } from '@src/components/view/main';
+import { useDispatch } from '@src/contexts/calendarStore';
+import { cls } from '@src/util/cssHelper';
+
+import { ProviderWrapper } from '@stories/util/providerWrapper';
+import { createRandomEventModelsForMonth } from '@stories/util/randomEvents';
+import { Story } from '@storybook/preact';
+
+export default { title: 'Main' };
+
+const style = {
+  position: 'absolute',
+  left: 0,
+  right: 0,
+  bottom: 5,
+  top: 5,
+};
+
+const Wrapper: FunctionComponent = ({ children }) => (
+  <div className={cls('layout')} style={style}>
+    {children}
+  </div>
+);
+
+const events = createRandomEventModelsForMonth();
+
+const Toolbar = () => {
+  const { changeView } = useDispatch('view');
+
+  return (
+    <div>
+      <button onClick={() => changeView('month')}>Month</button>
+      <button onClick={() => changeView('week')}>Week</button>
+      <button onClick={() => changeView('day')}>Day</button>
+    </div>
+  );
+};
+
+const Template: Story = (args) => (
+  <ProviderWrapper options={args.options} events={args.events}>
+    <Wrapper>
+      <Toolbar />
+      <Main />
+    </Wrapper>
+  </ProviderWrapper>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  events,
+};
