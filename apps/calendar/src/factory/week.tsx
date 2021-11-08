2bca37d2921ae7751388a7b69db8a83ddf89b683
Dohyung Ahn <dohyung.ahn@nhn.com>
2021-11-08 15:50:21 +0900

    chore: call `super` before initialization

--- apps/calendar/src/factory/week.tsx
+++ apps/calendar/src/factory/week.tsx
@@ -2,8 +2,13 @@ import { h } from 'preact';
 
 import WeekView from '@src/components/view/week';
 import CalendarControl from '@src/factory/calendarControl';
+import { Option } from '@src/model';
 
 export default class Week extends CalendarControl {
+  constructor(container: Element, options: Option = {}) {
+    super(container, options);
+  }
+
   protected getComponent() {
     return <WeekView />;
   }
