e8191f0ea587141bd3cb2485dfdd21e68d6cbc14
lja1018 <jaeeon.lim@nhn.com>
2021-11-10 17:40:07 +0900

    refactor: organize `math`

--- apps/calendar/src/util/math.ts
+++ apps/calendar/src/util/math.ts
@@ -1,40 +1,18 @@
-/**
- * Limit supplied value base on `minArr`, `maxArr`
- * @param {number} value - value
- * @param {array} minArr - min
- * @param {array} maxArr - max
- * @returns {number} limited value
- */
 export function limit(value: number, minArr: number[], maxArr: number[]) {
-  const v = Math.max(...[value].concat(minArr));
+  const v = Math.max(value, ...minArr);
 
-  return Math.min(...[v].concat(maxArr));
+  return Math.min(v, ...maxArr);
 }
 
 /**
- * Get ratio value.
- *
- * a : b = y : X;
- *
- * =
- *
- * X = (b * y) / a;
- * @param {number} a - a
- * @param {number} b - b
- * @param {number} y - y
- * @returns {number} ratio value
+ * a : b = y : x;
+ * ==
+ * x = (b * y) / a;
  */
 export function ratio(a: number, b: number, y: number) {
-  // a : b = y : x;
   return (b * y) / a;
 }
 
-/**
- * Whether value is between a and b
- * @param {number} value - value
- * @param {number} min - min value
- * @param {number} max - max value
- */
 export function isBetween(value: number, min: number, max: number) {
   return min <= value && value <= max;
 }
