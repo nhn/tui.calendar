aedd25bff29c0c0cb24a4cdf8530bf7dfd1f2b33
jajugoguma <daeyeon.kim@nhn.com>
2021-12-13 16:51:18 +0900

    test: add test for selection in month view

--- apps/calendar/playwright/month.spec.ts
+++ apps/calendar/playwright/month.spec.ts
@@ -1,10 +1,31 @@
 import { expect, test } from '@playwright/test';
 
+const MONTH_VIEW_PAGE_URL =
+  'http://localhost:6006/iframe.html?id=monthview--fixed-events&args=&viewMode=story';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto(MONTH_VIEW_PAGE_URL);
+});
+
 test('basic test', async ({ page }) => {
-  await page.goto(
-    'http://localhost:6006/iframe.html?id=monthview--fixed-events&args=&viewMode=story'
-  );
   const events = await page.$$('.toastui-calendar-weekday-event');
 
   expect(events).toHaveLength(4);
+});
+
+test.describe('Selection', () => {
+  test('select 2 cells from left to right', async ({ page }) => {
+    const startCellLocator = page.locator('.toastui-calendar-daygrid-cell').nth(31);
+    const cellBoundingBox = await startCellLocator.boundingBox();
+
+    await page.dragAndDrop(
+      '.toastui-calendar-daygrid-cell >> nth=31',
+      '.toastui-calendar-daygrid-cell >> nth=32'
+    );
+
+    const selectionLocator = page.locator('.toastui-calendar-daygrid-grid-selection');
+    const selectionBoundingBox = await selectionLocator.boundingBox();
+
+    expect(selectionBoundingBox.width).toBeGreaterThanOrEqual(cellBoundingBox.width * 2);
+  });
 });
