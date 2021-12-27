e5959cc36485f187b22b7be40d60fee46ab08e4b
lja1018 <jaeeon.lim@nhn.com>
2021-12-27 15:05:19 +0900

    test: serve storybook when running playwright tests
    * run playwright test for CI environment

--- apps/calendar/playwright/configs.ts
+++ apps/calendar/playwright/configs.ts
@@ -1,5 +1,6 @@
-export const WEEK_VIEW_PAGE_URL =
-  'http://localhost:6006/iframe.html?id=weekview--fixed-events&args=&viewMode=story';
+// eslint-disable-next-line no-process-env
+const PORT = process.env.CI ? 8080 : 6006;
 
-export const MONTH_VIEW_PAGE_URL =
-  'http://localhost:6006/iframe.html?id=monthview--fixed-events&args=&viewMode=story';
+export const WEEK_VIEW_PAGE_URL = `http://localhost:${PORT}/iframe.html?id=weekview--fixed-events&args=&viewMode=story`;
+
+export const MONTH_VIEW_PAGE_URL = `http://localhost:${PORT}/iframe.html?id=monthview--fixed-events&args=&viewMode=story`;
