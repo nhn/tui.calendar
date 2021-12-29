6c576357ca5e94804d2c4b381c93e3bbb820a0e1
Dohyung Ahn <dohyung.ahn@nhn.com>
2021-12-29 15:48:29 +0900

    feat: implement `useMergedRef`

---
+++ apps/calendar/src/hooks/common/mergedRef.ts
@@ -0,0 +1,27 @@
+import { Ref, RefObject } from 'preact';
+import { useMemo, useRef } from 'preact/hooks';
+
+export function useMergedRef<T>(...refs: Ref<T>[]): RefObject<T> {
+  const stableRef = useRef<T | null>(null);
+
+  return useMemo(
+    () => ({
+      get current() {
+        return stableRef.current;
+      },
+      set current(el) {
+        stableRef.current = el;
+        refs.forEach((ref) => {
+          if (ref) {
+            if (typeof ref === 'function') {
+              ref(el);
+            } else {
+              ref.current = el;
+            }
+          }
+        });
+      },
+    }),
+    [refs]
+  );
+}
