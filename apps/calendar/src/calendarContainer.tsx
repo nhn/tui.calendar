246ae91541e2be068086c4af3e8c1bb2510c37f8
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-01-06 17:32:58 +0900

    feat: implement event bus (#980)

--- apps/calendar/src/calendarContainer.tsx
+++ apps/calendar/src/calendarContainer.tsx
@@ -2,21 +2,31 @@ import { FunctionComponent, h } from 'preact';
 
 import FloatingLayer from '@src/components/floatingLayer';
 import { StoreProvider } from '@src/contexts/calendarStore';
+import { EventBusProvider } from '@src/contexts/eventBus';
 import { ThemeProvider } from '@src/contexts/theme';
 import Theme from '@src/theme';
+import { EventBus } from '@src/utils/eventBus';
 
 import { CalendarStore, InternalStoreAPI } from '@t/store';
 
 interface Props {
   theme: Theme;
   store: InternalStoreAPI<CalendarStore>;
+  eventBus: EventBus<ExternalEventTypes>;
 }
 
-export const CalendarContainer: FunctionComponent<Props> = ({ theme, store, children }) => (
-  <ThemeProvider theme={theme}>
-    <StoreProvider store={store}>
-      {children}
-      <FloatingLayer />
-    </StoreProvider>
-  </ThemeProvider>
+export const CalendarContainer: FunctionComponent<Props> = ({
+  theme,
+  store,
+  eventBus,
+  children,
+}) => (
+  <EventBusProvider value={eventBus}>
+    <ThemeProvider theme={theme}>
+      <StoreProvider store={store}>
+        {children}
+        <FloatingLayer />
+      </StoreProvider>
+    </ThemeProvider>
+  </EventBusProvider>
 );
