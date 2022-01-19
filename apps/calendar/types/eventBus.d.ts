74b1898dbf148a33cb837b0b0b529b97829744a7
lja1018 <jaeeon.lim@nhn.com>
2022-01-19 17:32:00 +0900

    refactor: change parameter type of clickDayname

--- apps/calendar/types/eventBus.d.ts
+++ apps/calendar/types/eventBus.d.ts
@@ -11,10 +11,14 @@ interface SelectDateTimeInfo {
   nativeEvent?: MouseEvent;
 }
 
+interface DaynameInfo {
+  date: string;
+}
+
 type ExternalEventTypes = {
   selectDateTime: (info: SelectDateTimeInfo) => void;
   beforeCreateEvent: (event: EventModelData) => void;
   beforeUpdateEvent: (event: EventModel, changes: EventModelData) => void;
-  clickDayname: ({ date }: { date: string }) => void;
+  clickDayname: (daynameInfo: DaynameInfo) => void;
   [eventName: string]: AnyFunc;
 };
