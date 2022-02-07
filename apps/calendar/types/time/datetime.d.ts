b70a05fd5ad37d8c269e1e795e8dd88762239f85
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-02-07 10:07:31 +0900

    refactor: specify `FormattedTimeString` type

--- apps/calendar/types/time/datetime.d.ts
+++ apps/calendar/types/time/datetime.d.ts
@@ -19,5 +19,5 @@ interface CellInfo extends CellStyle {
   date: TZDate;
 }
 
-type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-export type FormattedTimeString = `${Digit}${Digit}:00:00`;
+type HoursString = `${2}${0 | 1 | 2 | 3 | 4}` | `${0 | 1}${0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9}`;
+export type FormattedTimeString = `${HoursString}:00`;
