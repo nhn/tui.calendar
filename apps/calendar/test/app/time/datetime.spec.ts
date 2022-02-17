7c8cf4b8ceeb353ed83564ca1d9840f9c4d37c30
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-02-17 17:31:29 +0900

    test: remove duplicate helper function

--- apps/calendar/test/app/time/datetime.spec.ts
+++ apps/calendar/test/app/time/datetime.spec.ts
@@ -1,20 +1,7 @@
 import TZDate from '@src/time/date';
 import * as dt from '@src/time/datetime';
 
-function createDate(y: number, M: number, d: number): TZDate {
-  const year = String(y);
-  let month = String(M);
-  let day = String(d);
-
-  if (month.length < 2) {
-    month = `0${month}`;
-  }
-  if (day.length < 2) {
-    day = `0${day}`;
-  }
-
-  return new TZDate(`${[year, month, day].join('-')}T00:00:00`);
-}
+import { createDate } from '@test/helper';
 
 describe('datetime', () => {
   describe('millisecondsTo()', () => {
