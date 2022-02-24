dc4435cbc8fccf9c683baa4a4b8de58faad96d44
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-02-24 09:30:00 +0900

    test: use `dragAndDrop` util function

--- apps/calendar/src/hooks/gridSelection/gridSelection.spec.tsx
+++ apps/calendar/src/hooks/gridSelection/gridSelection.spec.tsx
@@ -1,6 +1,6 @@
 import { h } from 'preact';
 
-import { fireEvent, screen } from '@testing-library/preact';
+import { screen } from '@testing-library/preact';
 import { renderHook } from '@testing-library/preact-hooks';
 import userEvent from '@testing-library/user-event';
 
@@ -9,6 +9,7 @@ import { EventBusProvider } from '@src/contexts/eventBus';
 import { createGridPositionFinder, createTimeGridData, getWeekDates } from '@src/helpers/grid';
 import { timeGridSelectionHelper } from '@src/helpers/gridSelection';
 import { useGridSelection } from '@src/hooks/gridSelection/gridSelection';
+import { dragAndDrop } from '@src/test/utils';
 import TZDate from '@src/time/date';
 import { EventBusImpl } from '@src/utils/eventBus';
 import { noop } from '@src/utils/noop';
@@ -95,22 +96,6 @@ describe('useGridSelection', () => {
     }
 
     return result;
-  }
-  function dragMouse(container: HTMLElement, from: ClientMousePosition, to: ClientMousePosition) {
-    fireEvent.mouseDown(container, {
-      clientX: from.clientX,
-      clientY: from.clientY,
-    });
-    // to init drag
-    fireEvent.mouseMove(document, {
-      clientX: 0,
-      clientY: 0,
-    });
-    fireEvent.mouseMove(document, {
-      clientX: to.clientX,
-      clientY: to.clientY,
-    });
-    fireEvent.mouseUp(document);
   }
 
   beforeEach(() => {
@@ -317,7 +302,11 @@ describe('useGridSelection', () => {
         const container = screen.getByTestId('container');
 
         // When
-        dragMouse(container, { clientX: initX, clientY: initY }, { clientX: x, clientY: y });
+        dragAndDrop({
+          element: container,
+          initPosition: { clientX: initX, clientY: initY },
+          targetPosition: { clientX: x, clientY: y },
+        });
 
         // Then
         expect(result.current?.gridSelection).toEqual(expected);
@@ -345,7 +334,11 @@ describe('useGridSelection', () => {
       const container = screen.getByTestId('container');
 
       // When
-      dragMouse(container, { clientX: 35, clientY: 240 }, { clientX: 35, clientY: 480 });
+      dragAndDrop({
+        element: container,
+        initPosition: { clientX: 35, clientY: 240 },
+        targetPosition: { clientX: 35, clientY: 480 },
+      });
 
       // Then
       expect(showFormPopupAction).toHaveBeenCalledWith(
@@ -405,7 +398,11 @@ describe('useGridSelection', () => {
       const container = screen.getByTestId('container');
 
       // When
-      dragMouse(container, { clientX: 35, clientY: 240 }, { clientX: 35, clientY: 480 });
+      dragAndDrop({
+        element: container,
+        initPosition: { clientX: 35, clientY: 240 },
+        targetPosition: { clientX: 35, clientY: 480 },
+      });
 
       // Then
       expect(mockHandler).toHaveBeenCalledWith(
