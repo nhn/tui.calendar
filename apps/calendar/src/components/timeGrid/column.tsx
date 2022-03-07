418fd47adbd0b196c65648981ed9a70aaeb873a3
Heena Kwag <heena.kwag@nhn.com>
2022-03-07 17:27:48 +0900

    refactor: setRenderInfoOfUIModels in timeGrid

--- apps/calendar/src/components/timeGrid/column.tsx
+++ apps/calendar/src/components/timeGrid/column.tsx
@@ -5,7 +5,6 @@ import { useMemo } from 'preact/hooks';
 import { BackgroundEvent } from '@src/components/events/backgroundEvent';
 import { TimeEvent } from '@src/components/events/timeEvent';
 import { GridSelectionByColumn } from '@src/components/timeGrid/gridSelectionByColumn';
-import { setRenderInfoOfUIModels } from '@src/controller/column';
 import { getTopHeightByTime } from '@src/controller/times';
 import { cls, toPercent } from '@src/helpers/css';
 import { isBackgroundEvent } from '@src/model/eventModel';
@@ -14,7 +13,7 @@ import TZDate from '@src/time/date';
 import { setTimeStrToDate } from '@src/time/datetime';
 import { first, last } from '@src/utils/array';
 
-import { TimeGridRow } from '@t/grid';
+import { GridPositionFinder, TimeGridData } from '@t/grid';
 
 const classNames = {
   column: cls('column'),
@@ -56,42 +55,26 @@ function BackgroundEvents({
   );
 }
 
-function VerticalEvents({
-  events,
-  startTime,
-  endTime,
-}: {
-  events: EventUIModel[];
-  startTime: TZDate;
-  endTime: TZDate;
-}) {
+function VerticalEvents({ eventUIModels }: { eventUIModels: EventUIModel[] }) {
   // @TODO: use dynamic value
   const style = { marginRight: 8 };
-  const uiModels = useMemo(
-    () => setRenderInfoOfUIModels(events, startTime, endTime),
-    [endTime, events, startTime]
-  );
-  const verticalEvents = useMemo(
-    () =>
-      uiModels.map((uiModel) => (
-        <TimeEvent key={`${uiModel.valueOf()}-${uiModel.cid()}`} uiModel={uiModel} />
-      )),
-    [uiModels]
-  );
 
   return (
     <div className={classNames.events} style={style}>
-      {verticalEvents}
+      {eventUIModels.map((eventUIModel) => (
+        <TimeEvent key={`${eventUIModel.valueOf()}-${eventUIModel.cid()}`} uiModel={eventUIModel} />
+      ))}
     </div>
   );
 }
 
 interface Props {
-  timeGridRows: TimeGridRow[];
+  timeGridData: TimeGridData;
   columnDate: TZDate;
   columnWidth: string;
   columnIndex: number;
-  events: EventUIModel[];
+  totalUIModels: EventUIModel[][];
+  gridPositionFinder: GridPositionFinder;
   backgroundColor?: string;
   readOnly?: boolean;
 }
@@ -100,10 +83,13 @@ export const Column = memo(function Column({
   columnDate,
   columnWidth,
   columnIndex,
-  events,
-  timeGridRows,
+  totalUIModels,
+  gridPositionFinder,
+  timeGridData,
   backgroundColor,
 }: Props) {
+  const { rows: timeGridRows } = timeGridData;
+
   const [startTime, endTime] = useMemo(() => {
     const { startTime: startTimeStr } = first(timeGridRows);
     const { endTime: endTimeStr } = last(timeGridRows);
@@ -119,15 +105,17 @@ export const Column = memo(function Column({
     backgroundColor,
   };
 
+  const uiModelsByColumn = totalUIModels[columnIndex];
+
   return (
     <div
       className={classNames.column}
       style={style}
       data-testid={`timegrid-column-${columnDate.getDay()}`}
     >
-      <BackgroundEvents events={events} startTime={startTime} endTime={endTime} />
+      <BackgroundEvents events={uiModelsByColumn} startTime={startTime} endTime={endTime} />
       <GridSelectionByColumn columnIndex={columnIndex} timeGridRows={timeGridRows} />
-      <VerticalEvents events={events} startTime={startTime} endTime={endTime} />
+      <VerticalEvents eventUIModels={uiModelsByColumn} />
     </div>
   );
 });
