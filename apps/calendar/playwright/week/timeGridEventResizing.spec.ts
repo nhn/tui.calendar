923dbf386fda0fb7ae5c2d784568ebc29b4a055f
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-03-11 10:59:43 +0900

    wip: writing e2e test

--- apps/calendar/playwright/week/timeGridEventResizing.spec.ts
+++ apps/calendar/playwright/week/timeGridEventResizing.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test';
 
+import type { FormattedTimeString } from '../../types/time/datetime';
 import { WEEK_VIEW_PAGE_URL } from '../configs';
 import { dragAndDrop, getBoundingBox, getTimeGridLineLocator } from '../utils';
 
@@ -12,29 +13,63 @@ const TARGET_EVENT_TITLE = 'short time event';
 // And it starts from 04:00.
 const targetEventSelector = `[data-testid*="time-event-${TARGET_EVENT_TITLE}"]`;
 
-test('resize event to the bottom', async ({ page }) => {
-  // Given
-  const eventLocator = page.locator(targetEventSelector);
-  const resizeHandlerLocator = eventLocator.locator('[class*="resize-handler"]');
-  const targetRowLocator = page.locator(getTimeGridLineLocator('08:00'));
-  const targetColumnLocator = page.locator(`data-testid=timegrid-column-4`);
-  const eventBoundingBoxBeforeResize = await getBoundingBox(eventLocator);
+const cases: {
+  name: string;
+  targetEndTime: FormattedTimeString;
+  compareAssertion: Extract<keyof jest.Matchers<number>, 'toBeGreaterThan' | 'toBeLessThan'>;
+}[] = [
+  {
+    name: 'resize event to the bottom',
+    targetEndTime: '08:00',
+    compareAssertion: 'toBeGreaterThan',
+  },
+  {
+    name: 'resize event to the top',
+    targetEndTime: '05:00',
+    compareAssertion: 'toBeLessThan',
+  },
+];
 
-  const targetColumnBoundingBox = await getBoundingBox(targetColumnLocator);
+/**
+ * 1. 이벤트는 리사이즈가 시작된 컬럼 안에서만 상하로 크기를 조정할 수 있다.
+ * 2. 리사이즈가 완료되었을 때 전체 이벤트의 길이가 24시간이 넘어간다면 TimeGrid에서 제거되고 Allday 이벤트로 표시된다.
+ */
 
-  // When
-  await dragAndDrop(resizeHandlerLocator, targetRowLocator, {
-    sourcePosition: {
-      x: 1,
-      y: 1,
-    },
-    targetPosition: {
-      y: 1,
-      x: targetColumnBoundingBox.x - 1,
-    },
+test.describe.only('timegrid event resizing', () => {
+  cases.forEach(({ name, targetEndTime, compareAssertion }) => {
+    test(`${name}`, async ({ page }) => {
+      // Given
+      const eventLocator = page.locator(targetEventSelector);
+      const resizeHandlerLocator = eventLocator.locator('[class*="resize-handler"]');
+      const targetRowLocator = page.locator(getTimeGridLineLocator(targetEndTime));
+      const targetColumnLocator = page.locator(`data-testid=timegrid-column-3`);
+      const eventBoundingBoxBeforeResize = await getBoundingBox(eventLocator);
+
+      const targetColumnBoundingBox = await getBoundingBox(targetColumnLocator);
+
+      // When
+      await dragAndDrop(resizeHandlerLocator, targetRowLocator, {
+        sourcePosition: {
+          x: 1,
+          y: 1,
+        },
+        targetPosition: {
+          y: 1,
+          x: targetColumnBoundingBox.x + 1,
+        },
+      });
+
+      // Then
+      const eventBoundingBoxAfterResize = await getBoundingBox(eventLocator);
+      expect(eventBoundingBoxAfterResize.height)[compareAssertion](
+        eventBoundingBoxBeforeResize.height
+      );
+
+      const targetRowBoundingBox = await getBoundingBox(targetRowLocator);
+      expect(eventBoundingBoxAfterResize.y + eventBoundingBoxAfterResize.height).toBeCloseTo(
+        targetRowBoundingBox.y,
+        -1
+      );
+    });
   });
-
-  // Then
-  const eventBoundingBoxAfterResize = await getBoundingBox(eventLocator);
-  expect(eventBoundingBoxAfterResize.height).toBeGreaterThan(eventBoundingBoxBeforeResize.height);
 });
