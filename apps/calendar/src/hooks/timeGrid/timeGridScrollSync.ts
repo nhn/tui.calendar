76dac53e8ec6a160ce55cf14ec6ed694607b6225
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-03-15 17:40:17 +0900

    feat: implement `useTimeGridScrollSync` hook

---
+++ apps/calendar/src/hooks/timeGrid/timeGridScrollSync.ts
@@ -0,0 +1,28 @@
+import { useTransientUpdate } from '@src/hooks/common/transientUpdate';
+import { dndSelector } from '@src/selectors';
+import { DraggingState } from '@src/slices/dnd';
+import { isPresent } from '@src/utils/type';
+
+export function useTimeGridScrollSync(scrollArea: HTMLDivElement | null, rowCount: number) {
+  useTransientUpdate(dndSelector, ({ y, draggingItemType, draggingState }) => {
+    if (
+      isPresent(scrollArea) &&
+      draggingItemType?.startsWith('event/timeGrid') &&
+      draggingState === DraggingState.DRAGGING &&
+      isPresent(y)
+    ) {
+      const { offsetTop, offsetHeight, scrollHeight } = scrollArea;
+      // Set minimum scroll boundary to the height of one row.
+      const scrollBoundary = Math.floor(scrollHeight / rowCount);
+      const layoutHeight = offsetTop + offsetHeight;
+      // arbitrary value to move scroll (px).
+      const SCROLL_OFFSET = 10;
+
+      if (y < offsetTop + scrollBoundary) {
+        scrollArea.scrollBy(0, -SCROLL_OFFSET);
+      } else if (y > layoutHeight - scrollBoundary) {
+        scrollArea.scrollBy(0, SCROLL_OFFSET);
+      }
+    }
+  });
+}
