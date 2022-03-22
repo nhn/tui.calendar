cfabc0df53ed20d75671a8143079b524aee38c90
lja1018 <jaeeon.lim@nhn.com>
2022-03-22 17:20:56 +0900

    refactor: remove unused `convertPxToNum`

--- apps/calendar/src/helpers/css.ts
+++ apps/calendar/src/helpers/css.ts
@@ -47,14 +47,3 @@ export function cls(...args: ClassNameValue[]): string {
 export function toPercent(value: number) {
   return `${value}%`;
 }
-
-export function convertPxToNum(pxString: string) {
-  const isPxString = /^\d+px$/.test(pxString);
-  if (!isPxString) {
-    throw new Error(
-      '[convertPxToNum] you should pass a pixel string value as argument - i.e., "18px"'
-    );
-  }
-
-  return parseFloat(pxString);
-}
