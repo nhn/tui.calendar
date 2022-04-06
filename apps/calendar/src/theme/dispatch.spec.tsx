8b4528ada607227de7b1188fe2850bc04a49e002
lja1018 <jaeeon.lim@nhn.com>
2022-04-06 15:21:24 +0900

    test: add tests for `setWeekTheme`

--- apps/calendar/src/theme/dispatch.spec.tsx
+++ apps/calendar/src/theme/dispatch.spec.tsx
@@ -1,7 +1,7 @@
 import { h } from 'preact';
 
 import { initThemeStore, useTheme } from '@src/contexts/themeStore';
-import { commonThemeSelector } from '@src/selectors/theme';
+import { commonThemeSelector, weekThemeSelector } from '@src/selectors/theme';
 import { act, render, screen } from '@src/test/utils';
 
 describe('setCommonTheme', () => {
@@ -10,7 +10,7 @@ describe('setCommonTheme', () => {
   let beforeBackgroundColor: string;
   let beforeGridSelectionBorder: string;
 
-  function WeekThemeComponent() {
+  function CommonThemeComponent() {
     const {
       backgroundColor,
       gridSelection: { border },
@@ -27,7 +27,7 @@ describe('setCommonTheme', () => {
   beforeEach(() => {
     beforeBackgroundColor = theme.getState().common?.backgroundColor ?? '';
     beforeGridSelectionBorder = theme.getState().common?.gridSelection.border ?? '';
-    render(<WeekThemeComponent />, { theme });
+    render(<CommonThemeComponent />, { theme });
   });
 
   it('should set theme and other properties should not be changed', () => {
@@ -77,5 +77,85 @@ describe('setCommonTheme', () => {
     // Then
     expect(screen.getByText(`backgroundColor: ${beforeBackgroundColor}`)).toBeInTheDocument();
     expect(screen.getByText(`border: ${beforeGridSelectionBorder}`)).toBeInTheDocument();
+  });
+});
+
+describe('setWeekTheme', () => {
+  const theme = initThemeStore();
+  const { setWeekTheme } = theme.getState().dispatch;
+  let beforePastTimeColor: string;
+  let beforeCurrentTimeColor: string;
+
+  function WeekThemeComponent() {
+    const {
+      pastTime: { color: pastTimeColor },
+      currentTime: { color: currentTimeColor },
+    } = useTheme(weekThemeSelector);
+
+    return (
+      <div>
+        <div>pastTimeColor: {pastTimeColor}</div>
+        <div>currentTimeColor: {currentTimeColor}</div>
+      </div>
+    );
+  }
+
+  beforeEach(() => {
+    beforePastTimeColor = theme.getState().week?.pastTime.color ?? '';
+    beforeCurrentTimeColor = theme.getState().week?.currentTime.color ?? '';
+    render(<WeekThemeComponent />, { theme });
+  });
+
+  it('should set theme and other properties should not be changed', () => {
+    // Given
+    const color = 'black';
+
+    // When
+    act(() => {
+      setWeekTheme({
+        pastTime: {
+          color,
+        },
+      });
+    });
+
+    // Then
+    expect(theme.getState().week?.pastTime.color).toBe(color);
+    expect(theme.getState().week?.currentTime.color).toBe(beforeCurrentTimeColor);
+  });
+
+  it('should rerender component that use useTheme when theme is changed', () => {
+    // Given
+    const color = 'black';
+
+    // When
+    act(() => {
+      setWeekTheme({
+        pastTime: {
+          color,
+        },
+      });
+    });
+
+    // Then
+    expect(screen.getByText(`pastTimeColor: ${color}`)).toBeInTheDocument();
+  });
+
+  it('should not change theme when unused theme is changed', () => {
+    // Given
+    const color = 'black';
+
+    // When
+    act(() => {
+      setWeekTheme({
+        today: {
+          color,
+        },
+      });
+    });
+
+    // Then
+    expect(screen.getByText(`pastTimeColor: ${beforePastTimeColor}`)).toBeInTheDocument();
+    expect(screen.getByText(`currentTimeColor: ${beforeCurrentTimeColor}`)).toBeInTheDocument();
   });
 });
