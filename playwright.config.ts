a98ce397d1f15088a80919e2f1a4cb138a0b7324
lja1018 <jaeeon.lim@nhn.com>
2022-04-13 14:53:49 +0900

    test: add parallel test options

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { PlaywrightTestConfig, devices } from '@playwright/test';
 const config: PlaywrightTestConfig = {
   testDir: 'apps/calendar/playwright',
   testMatch: '*.e2e.ts',
+  fullyParallel: true,
   timeout: 30000,
   forbidOnly: !!process.env.CI,
   workers: process.env.CI ? 2 : undefined,
