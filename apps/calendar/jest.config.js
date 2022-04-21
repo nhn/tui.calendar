9af82139c8ea6dcff542480ccdb9c8740a0082e0
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-04-21 16:12:54 +0900

    test: define css mock file

--- apps/calendar/jest.config.js
+++ apps/calendar/jest.config.js
@@ -4,8 +4,8 @@ module.exports = {
   preset: 'ts-jest',
   moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node', 'd.ts'],
   moduleNameMapper: {
+    '\\.(css)$': '<rootDir>/src/test/cssFileMock.ts',
     '^@src/(.*)$': '<rootDir>/src/$1',
-    '\\.(css)$': '<rootDir>/__mocks__/styleMock.ts',
   },
   globals: { 'ts-jest': { tsconfig: '<rootDir>/tsconfig.test.json' } },
   watchPathIgnorePatterns: ['<rootDir>/.storybook', '<rootDir>/.stories', '/node_modules/'],
