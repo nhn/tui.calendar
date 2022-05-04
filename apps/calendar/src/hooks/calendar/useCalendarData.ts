3027abc03636e8eb517e8e154b7e17d5663278c6
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-05-04 15:42:23 +0900

    refactor: integrate `useEventWithTimezone` in `useCalendarData`

--- apps/calendar/src/hooks/calendar/useCalendarData.ts
+++ apps/calendar/src/hooks/calendar/useCalendarData.ts
@@ -1,5 +1,6 @@
 import { useMemo } from 'preact/hooks';
 
+import { useEventsWithTimezone } from '@src/hooks/timezone/useEventsWithTimezone';
 import type EventModel from '@src/model/eventModel';
 import type { Filter } from '@src/utils/collection';
 import Collection from '@src/utils/collection';
@@ -7,11 +8,18 @@ import Collection from '@src/utils/collection';
 import type { CalendarData } from '@t/events';
 
 export function useCalendarData(calendar: CalendarData, ...filters: Filter<EventModel>[]) {
+  const filteredEvents = useMemo(
+    () => calendar.events.filter(Collection.and<EventModel>(...filters)),
+    [calendar.events, filters]
+  );
+
+  const filteredEventsWithTimezone = useEventsWithTimezone(filteredEvents);
+
   return useMemo(
     () => ({
       ...calendar,
-      events: calendar.events.filter(Collection.and<EventModel>(...filters)),
+      events: filteredEventsWithTimezone,
     }),
-    [calendar, filters]
+    [calendar, filteredEventsWithTimezone]
   );
 }
