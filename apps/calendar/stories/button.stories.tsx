6f5fb9a06c70ba8c2aa8eea480d71ecd2a18c227
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-05-09 11:30:25 +0900

    chore: remove unused button component

--- apps/calendar/stories/button.stories.tsx
+++
@@ -1,8 +0,0 @@
-import { h } from 'preact';
-
-import { Button } from '@src/components/ui/button';
-
-export default { title: 'Components/Button', component: Button };
-
-export const withText = () => <Button className="button">Text Button</Button>;
-export const withEmoji = () => <Button className="button">😀 😎 👍 💯</Button>;
