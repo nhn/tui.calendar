ea3332212f4801dd669c15e01d261fc650bea87c
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-05-10 12:07:09 +0900

    env: update pre-commit scripts

---
+++ apps/calendar/.lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "**/*.{js}": "eslint --fix",
+  "**/*.{ts,tsx}": [
+    () => "npm run check-types --workspace=@toast-ui/calendar",
+    "eslint --fix"
+  ],
+  "**/*.css": "stylelint"
+}
