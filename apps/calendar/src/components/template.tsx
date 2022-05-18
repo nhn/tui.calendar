bd95ecb9c77a57d5e98b6ae0eef301a7ea883fef
lja1018 <jaeeon.lim@nhn.com>
2022-05-18 14:47:52 +0900

    refactor: template component return null if templateFunc not found

--- apps/calendar/src/components/template.tsx
+++ apps/calendar/src/components/template.tsx
@@ -5,7 +5,7 @@ import { cls } from '@src/helpers/css';
 import { templateSelector } from '@src/selectors';
 import type { TemplateName } from '@src/template/default';
 import { sanitize } from '@src/utils/sanitizer';
-import { isString } from '@src/utils/type';
+import { isNil, isString } from '@src/utils/type';
 
 import type { TemplateReturnType } from '@t/template';
 
@@ -15,13 +15,14 @@ interface Props {
   as?: keyof HTMLElementTagNameMap;
 }
 
-function identity(value: unknown) {
-  return value as TemplateReturnType;
-}
-
 export function Template({ template, model, as: tagName = 'div' }: Props) {
   const templates = useStore(templateSelector);
-  const templateFunc: Function = templates[template] || identity;
+  const templateFunc: Function = templates[template];
+
+  if (isNil(templateFunc)) {
+    return null;
+  }
+
   const htmlOrVnode: TemplateReturnType = templateFunc(model, h);
 
   return isString(htmlOrVnode)
