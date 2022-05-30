c76cb195492aa3c53e467ef9145fefb24969b88f
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-05-30 12:22:57 +0900

    chore: remove dispatch selection by array of groups
    * this will always invoke re-render
      because `pick` function always returns new object

--- apps/calendar/src/contexts/calendarStore.ts
+++ apps/calendar/src/contexts/calendarStore.ts
@@ -14,7 +14,6 @@ import { createViewDispatchers, createViewSlice } from '@src/slices/view';
 import { createStoreContext } from '@src/store';
 import { devtools } from '@src/store/devtool';
 import { createStore } from '@src/store/internal';
-import { pick } from '@src/utils/object';
 
 import type { Options } from '@t/options';
 import type { CalendarStore, Dispatchers, SetState, StoreCreator } from '@t/store';
@@ -59,18 +58,12 @@ export { StoreProvider, useInternalStore, useStore };
 
 export function useDispatch(): Dispatchers;
 export function useDispatch<Group extends keyof Dispatchers>(group: Group): Dispatchers[Group];
-export function useDispatch<Group extends keyof Dispatchers>(
-  group: Group[]
-): Pick<Dispatchers, Group>;
-export function useDispatch<Group extends keyof Dispatchers>(group?: Group | Group[]) {
+export function useDispatch<Group extends keyof Dispatchers>(group?: Group) {
   return useStore(
     useCallback(
       (state) => {
         if (!group) {
           return state.dispatch;
-        }
-        if (Array.isArray(group)) {
-          return pick(state.dispatch, ...group);
         }
 
         return state.dispatch[group];
