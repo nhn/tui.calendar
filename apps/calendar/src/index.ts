51ba0da0368ff55bcba2ee8a397ce2bac3daf0d7
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-05-30 10:26:31 +0900

    feat: export `TZDate` class

--- apps/calendar/src/index.ts
+++ apps/calendar/src/index.ts
@@ -2,7 +2,8 @@ import Calendar from '@src/factory/calendar';
 import Day from '@src/factory/day';
 import Month from '@src/factory/month';
 import Week from '@src/factory/week';
+import TZDate from '@src/time/date';
 
 export default Calendar;
 
-export { Day, Month, Week };
+export { Day, Month, TZDate, Week };
