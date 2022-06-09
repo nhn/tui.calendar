74b30a031c7da3757e93eee8cca0416276988776
곽희나 <heena.kwag@nhn.com>
2022-06-09 18:03:42 +0900

    feat: export EventObject in the index.ts

--- apps/calendar/src/index.ts
+++ apps/calendar/src/index.ts
@@ -4,6 +4,9 @@ import Month from '@src/factory/month';
 import Week from '@src/factory/week';
 import TZDate from '@src/time/date';
 
+import type { EventObjectWithDefaultValues } from '@t/events';
+
 export default Calendar;
 
 export { Day, Month, TZDate, Week };
+export type { EventObjectWithDefaultValues as EventObject };
