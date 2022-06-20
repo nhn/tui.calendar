f144a891e85103e8f47f8f2359692f77afe248a7
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-06-20 21:23:39 +0900

    docs: create JSDoc of CalendarCore and TZDate

--- apps/calendar/src/factory/calendarCore.tsx
+++ apps/calendar/src/factory/calendarCore.tsx
@@ -33,12 +33,140 @@ import type {
 } from '@t/store';
 import type { ThemeState, ThemeStore } from '@t/theme';
 
+/**
+ * {@link https://nhn.github.io/tui.code-snippet/latest/CustomEvents CustomEvents} document at {@link https://github.com/nhn/tui.code-snippet tui-code-snippet}
+ * @typedef {CustomEvents} CustomEvents
+ */
+
+/**
+ * Define Calendars to group events.
+ *
+ * @typedef {object} CalendarInfo
+ * @property {string} id - Calendar id.
+ * @property {string} name - Calendar name.
+ * @property {string} color - Text color of events.
+ * @property {string} borderColor - Left border color of events.
+ * @property {string} backgroundColor - Background color of events.
+ * @property {string} dragBackgroundColor - Background color of events during dragging.
+ */
+
+/**
+ * Timezone options of the calendar instance.
+ *
+ * For more information, see {@link TODO-GitHub-Options|Timezone options} in guide.
+ *
+ * @typedef {object} TimezoneOptions
+ * @example
+ * const calendar = new Calendar('#container', {
+ *   timezone: {
+ *     // @property {string} zones[].timezoneName - Timezone name. it should be one of IANA timezone names.
+ *     // @property {string} [zones[].displayLabel] - Display label of timezone.
+ *     // @property {string} [zones[].tooltip] - Tooltip of the element of the display label.
+ *     zones: [
+ *       {
+ *         timezoneName: 'Asia/Seoul',
+ *         displayLabel: 'UTC+9:00',
+ *         tooltip: 'Seoul'
+ *       },
+ *       {
+ *         timezoneName: 'Europe/London',
+ *         displayLabel: 'UTC+1:00',
+ *         tooltip: 'BST'
+ *       }
+ *     ],
+ *     // This function will be called for rendering components for each timezone.
+ *     // You don't have to use it if you're able to `Intl.DateTimeFormat` API with `timeZone` option.
+ *     // this function should return timezone offset from UTC.
+ *     // for instance, using moment-timezone:
+ *     customOffsetCalculator: (timezoneName, timestamp) => {
+ *       return moment.tz(timezoneName).utcOffset(timestamp);
+ *     }
+ *   }
+ * });
+ * @property {Array.<object>} zones - Timezone data.
+ * @property {string} zones[].timezoneName - Timezone name. it should be one of IANA timezone names.
+ * @property {string} [zones[].displayLabel] - Display label of timezone.
+ * @property {string} [zones[].tooltip] - Tooltip of the element of the display label.
+ * @property {function} customOffsetCalculator - Custom offset calculator when you're not able to leverage `Intl.DateTimeFormat` API.
+ */
+
+/**
+ * Object to create/modify events.
+ * @typedef {object} EventObject
+ * @property {string} [id] - Event id.
+ * @property {string} [calendarId] - Calendar id.
+ * @property {string} [title] - Event title.
+ * @property {string} [body] - Body content of the event.
+ * @property {string} [isAllday] - Whether the event is all day or not.
+ * @property {string|number|Date|TZDate} [start] - Start time of the event.
+ * @property {string|number|Date|TZDate} [end] - End time of the event.
+ * @property {number} [goingDuration] - Travel time which is taken to go.
+ * @property {number} [comingDuration] - Travel time which is taken to come back.
+ * @property {string} [location] - Location of the event.
+ * @property {Array.<string>} [attendees] - Attendees of the event.
+ * @property {string} [category] - Category of the event. Available categories are 'milestone', 'task', 'time' and 'allday'.
+ * @property {string} [dueDateClass] - Classification of work events. (before work, before lunch, before work)
+ * @property {string} [recurrenceRule] - Recurrence rule of the event.
+ * @property {string} [state] - State of the event. Available states are 'Busy', 'Free'.
+ * @property {boolean} [isVisible] - Whether the event is visible or not.
+ * @property {boolean} [isPending] - Whether the event is pending or not.
+ * @property {boolean} [isFocused] - Whether the event is focused or not.
+ * @property {boolean} [isReadOnly] - Whether the event is read only or not.
+ * @property {boolean} [isPrivate] - Whether the event is private or not.
+ * @property {string} [color] - Text color of the event.
+ * @property {string} [backgroundColor] - Background color of the event.
+ * @property {string} [dragBackgroundColor] - Background color of the event during dragging.
+ * @property {string} [borderColor] - Left border color of the event.
+ * @property {object} [customStyle] - Custom style of the event. the key of CSS property should be camelCase (e.g. {'fontSize': '12px'})
+ * @property {*} [raw] - Raw data of the event. it's an arbitrary property for anything.
+ */
+
+/**
+ * CalendarCore class
+ *
+ * @class CalendarCore
+ * @mixes CustomEvents
+ * @param {string|Element} container - container element or selector.
+ * @param {object} options - calendar options. For more information, see {@link TODO-GitHub-Options|Calendar options} in guide.
+ *   @param {string} [options.defaultView="week"] - Initial view type. Available values are: 'day', 'week', 'month'.
+ *   @param {boolean} [options.useFormPopup=false] - Whether to use the default form popup when creating/modifying events.
+ *   @param {boolean} [options.useDetailPopup=false] - Whether to use the default detail popup when clicking events.
+ *   @param {boolean} [options.isReadOnly=false] - Whether the calendar is read-only.
+ *   @param {boolean} [options.usageStatistics=true] - Whether to allow collect hostname and send the information to google analytics.
+ *                                              For more information, check out the {@link https://github.com/nhn/tui.calendar/tree/main/apps/calendar#collect-statistics-on-the-use-of-open-source|documentation}.
+ *   @param {function} [options.eventFilter] - A function that returns true if the event should be displayed. the default filter checks if the event's `isVisible` property is true.
+ *   @param {object} [options.week] - Week option of the calendar instance.
+ *     @param {number} [options.week.startDayOfWeek=0] - Start day of the week. Available values are 0 (Sunday) to 6 (Saturday).
+ *     @param {Array.<string>} [options.week.dayNames] - Names of days of the week. Should be 7 items starting from Sunday to Saturday. If not specified, the default names are used.
+ *                                               Default values are ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].
+ *     @param {boolean} [options.week.workweek=false] - Whether to exclude Saturday and Sunday.
+ *     @param {boolean} [options.week.showTimezoneCollapseButton=true] - Whether to show the timezone collapse button.
+ *     @param {boolean} [options.week.timezonesCollapsed=false] - Whether to collapse the timezones.
+ *     @param {number} [options.week.hourStart=0] - Start hour of the day. Available values are 0 to 24.
+ *     @param {number} [options.week.hourEnd=24] - End hour of the day. Available values are 0 to 24. Must be greater than `hourStart`.
+ *     @param {boolean} [options.week.narrowWeekend=false] - Whether to narrow down width of weekends to half.
+ *     @param {boolean|Array.<string>} [options.week.eventView=true] - Determine which view to display events. Available values are 'allday' and 'time'. set to `false` to disable event view.
+ *     @param {boolean|Array.<string>} [options.week.taskView=true] - Determine which view to display tasks. Available values are 'milestone' and 'task'. set to `false` to disable task view.
+ *   @param {object} options.month - Month option of the calendar instance.
+ *     @param {number} [options.month.startDayOfWeek=0] - Start day of the week. Available values are 0 (Sunday) to 6 (Saturday).
+ *     @param {Array.<string>} [options.month.dayNames] - Names of days of the week. Should be 7 items starting from Sunday to Saturday. If not specified, the default names are used.
+ *                                                Default values are ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'].
+ *     @param {boolean} [options.month.workweek=false] - Whether to exclude Saturday and Sunday.
+ *     @param {boolean} [options.month.narrowWeekend=false] - Whether to narrow down width of weekends to half.
+ *     @param {number} [options.month.visibleWeeksCount=0] - Number of weeks to display. 0 means display all weeks.
+ *   @param {Array.<CalendarInfo>} [options.calendars] - Calendars to group events.
+ *   @param {boolean|object} [options.gridSelection=true] - Whether to enable grid selection. or it's option. it's enabled when the value is `true` and object and will be disabled when `isReadOnly` is true.
+ *     @param {boolean} options.gridSelection.enableDbClick - Whether to enable double click to select area.
+ *     @param {boolean} options.gridSelection.enableClick - Whether to enable click to select area.
+ *   @param {TimezoneOptions} options.timezone - Timezone option of the calendar instance. For more information about timezone, check out the {@link TODO-GitHub-Options|Options} in guide.
+ *   @param {Theme} options.theme - Theme option of the calendar instance. For more information, see {@link TODO-GitHub-Theme|Theme} in guide.
+ *   @param {TemplateConfig} options.template - Template option of the calendar instance. For more information, see {@link TODO-GitHub-Template|Template} in guide.
+ */
 export default abstract class CalendarCore implements EventBus<ExternalEventTypes> {
   protected container: Element | null;
 
   /**
    * start and end date of weekly, monthly
-   * @type {object}
    * @private
    */
   protected renderRange: {
@@ -241,7 +369,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
 
   /**
    * Create events and render calendar.
-   * @param {EventObject[]} events - list of {@link EventObject}
+   * @param {Array.<EventObject>} events - list of {@link EventObject}
    * @example
    * calendar.createEvents([
    *   {
@@ -283,7 +411,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
    *
    * @param {string} eventId - event's id
    * @param {string} calendarId - calendar's id of the event
-   * @returns {EventObject | null} event. If the event can't be found, it returns null.
+   * @returns {EventObject|null} event. If the event can't be found, it returns null.
    *
    * @example
    * const event = calendar.getEvent(eventId, calendarId);
@@ -339,7 +467,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   /**
    * Set events' visibility by calendar ID
    *
-   * @param {string|string[]} calendarId - The calendar id or ids to change visibility
+   * @param {string|Array.<string>} calendarId - The calendar id or ids to change visibility
    * @param {boolean} isVisible - If set to true, show the events. If set to false, hide the events.
    */
   setCalendarVisibility(calendarId: string | string[], isVisible: boolean) {
@@ -400,7 +528,6 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   }
 
   /**
-   * TODO: implement this
    * Scroll to current time on today in case of daily, weekly view.
    *
    * @example
@@ -461,7 +588,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   /**
    * Move to specific date.
    *
-   * @param {DateType} date - The date to move
+   * @param {Date|string|number|TZDate} date - The date to move. it should be eligible parameter to create a `Date` instance if `date` is string or number.
    * @example
    * calendar.on('clickDayName', (event) => {
    *   if (calendar.getViewName() === 'week') {
@@ -516,7 +643,11 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
    * Change color values of events belong to a certain calendar.
    *
    * @param {string} calendarId - The calendar ID
-   * @param {CalendarColor} colorOptions - The {@link CalendarColor} object
+   * @param {object} colorOptions - The color values of the calendar
+   *   @param {string} colorOptions.color - The text color of the events
+   *   @param {string} colorOptions.borderColor - Left border color of events
+   *   @param {string} colorOptions.backgroundColor - Background color of events
+   *   @param {string} colorOptions.dragBackgroundColor - Background color of events during dragging
    *
    * @example
    * calendar.setCalendarColor('1', {
@@ -547,7 +678,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   /**
    * Change current view type.
    *
-   * @param {ViewType} viewName - The new view name to change
+   * @param {string} viewName - The new view name to change to. Available values are 'month', 'week', 'day'.
    *
    * @example
    * // change to daily view
@@ -592,7 +723,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   /**
    * Set the theme of the calendar.
    *
-   * @param {DeepPartial<ThemeState>} theme - theme object
+   * @param {Theme} theme - The theme object to apply. For more information, see {@link TODO-GitHub-Theme|Theme} in guide.
    *
    * @example
    * calendar.setTheme({
@@ -620,9 +751,9 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   }
 
   /**
-   * Get current {@link Options}.
+   * Get current options.
    *
-   * @returns {Options} options
+   * @returns {Options} - The current options of the instance
    */
   getOptions() {
     const { options, template } = this.getStoreState();
@@ -636,11 +767,13 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   }
 
   /**
-   * Set options of calendar.
+   * Set options of calendar. For more information, see {@link TODO-GitHub-Options|Options} in guide.
    *
-   * @param {Options} options - set {@link Options}
+   * @param {Options} options - The options to set
    */
-  setOptions({ theme, template, ...restOptions }: Options) {
+  setOptions(options: Options) {
+    // destructure options here for tui.doc to generate docs correctly
+    const { theme, template, ...restOptions } = options;
     const { setTheme } = this.theme.getState().dispatch;
     const {
       options: { setOptions },
@@ -659,7 +792,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   }
 
   /**
-   * Get current rendered date.
+   * Get current rendered date. (see {@link TZDate} for further information)
    *
    * @returns {TZDate}
    */
@@ -670,7 +803,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   }
 
   /**
-   * Start time of rendered date range. ({@link TZDate} for further information)
+   * Start time of rendered date range. (see {@link TZDate} for further information)
    *
    * @returns {TZDate}
    */
@@ -679,7 +812,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   }
 
   /**
-   * End time of rendered date range. ({@link TZDate} for further information)
+   * End time of rendered date range. (see {@link TZDate} for further information)
    *
    * @returns {TZDate}
    */
@@ -690,7 +823,7 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
   /**
    * Get current view name('day', 'week', 'month').
    *
-   * @returns {ViewType} current view name
+   * @returns {string} current view name ('day', 'week', 'month')
    */
   getViewName(): ViewType {
     const { currentView } = this.getStoreState('view');
@@ -709,12 +842,11 @@ export default abstract class CalendarCore implements EventBus<ExternalEventType
     setCalendars(calendars);
   }
 
+  // TODO: specify position of popup
   /**
-   * TODO: specify position of popup
-   *
    * Open event form popup with predefined form values.
    *
-   * @param {EventObject} event - The preset {@link EventObject} data
+   * @param {EventObject} event - The predefined {@link EventObject} data to show in form.
    */
   openFormPopup(event: EventObject) {
     const { showFormPopup } = this.getStoreDispatchers().popup;
--- apps/calendar/src/time/date.ts
+++ apps/calendar/src/time/date.ts
@@ -13,18 +13,18 @@ function getTZOffsetMSDifference(offset: number) {
 }
 
 /**
- * Timezone Date Class
- * @param {number|TZDate|Date|string} date - date to be converted
+ * Custom Date Class to handle timezone offset.
+ *
+ * For more information, see {@link TODO-GitHub-TZDate|TZDate} in guide.
+ *
+ * @class TZDate
+ * @param {number|TZDate|Date|string} date - date value to be converted. If date is number or string, it should be eligible to parse by Date constructor.
  */
 export default class TZDate {
   private tzOffset: number | null = null;
 
   private d: DateInterface;
 
-  /**
-   * Timezone Date Class
-   * @param {number|TZDate|Date|string} date - date to be converted
-   */
   constructor(...args: any[]) {
     if (args[0] instanceof TZDate) {
       this.d = createDate(args[0].getTime());
@@ -33,46 +33,83 @@ export default class TZDate {
     }
   }
 
+  /**
+   * Get the string representation of the date.
+   * @returns {string} string representation of the date.
+   */
   toString() {
     return this.d.toString();
   }
 
+  /**
+   * Add years to the instance.
+   * @param {number} y - number of years to be added.
+   * @returns {TZDate} - returns the instance itself.
+   */
   addFullYear(y: number): TZDate {
     this.setFullYear(this.getFullYear() + y);
 
     return this;
   }
 
+  /**
+   * Add months to the instance.
+   * @param {number} m - number of months to be added.
+   * @returns {TZDate} - returns the instance itself.
+   */
   addMonth(m: number): TZDate {
     this.setMonth(this.getMonth() + m);
 
     return this;
   }
 
+  /**
+   * Add dates to the instance.
+   * @param {number} d - number of days to be added.
+   * @returns {TZDate} - returns the instance itself.
+   */
   addDate(d: number): TZDate {
     this.setDate(this.getDate() + d);
 
     return this;
   }
 
+  /**
+   * Add hours to the instance.
+   * @param {number} h - number of hours to be added.
+   */
   addHours(h: number): TZDate {
     this.setHours(this.getHours() + h);
 
     return this;
   }
 
+  /**
+   * Add minutes to the instance.
+   * @param {number} M - number of minutes to be added.
+   * @returns {TZDate} - returns the instance itself.
+   */
   addMinutes(M: number): TZDate {
     this.setMinutes(this.getMinutes() + M);
 
     return this;
   }
 
+  /**
+   * Add seconds to the instance.
+   * @param {number} s - number of seconds to be added.
+   */
   addSeconds(s: number): TZDate {
     this.setSeconds(this.getSeconds() + s);
 
     return this;
   }
 
+  /**
+   * Add milliseconds to the instance.
+   * @param {number} ms - number of milliseconds to be added.
+   * @returns {TZDate} - returns the instance itself.
+   */
   addMilliseconds(ms: number): TZDate {
     this.setMilliseconds(this.getMilliseconds() + ms);
 
@@ -80,6 +117,17 @@ export default class TZDate {
   }
 
   /* eslint-disable max-params*/
+  /**
+   * Set the date and time all at once.
+   * @param {number} y - year
+   * @param {number} m - month
+   * @param {number} d - date
+   * @param {number} h - hours
+   * @param {number} M - minutes
+   * @param {number} s - seconds
+   * @param {number} ms - milliseconds
+   * @returns {TZDate} - returns the instance itself.
+   */
   setWithRaw(y: number, m: number, d: number, h: number, M: number, s: number, ms: number): TZDate {
     this.setFullYear(y, m, d);
     this.setHours(h, M, s, ms);
@@ -87,14 +135,26 @@ export default class TZDate {
     return this;
   }
 
+  /**
+   * Convert the instance to the native `Date` object.
+   * @returns {Date} - The native `Date` object.
+   */
   toDate(): Date {
     return this.d.toDate();
   }
 
+  /**
+   * Get the value of the date. (milliseconds since 1970-01-01 00:00:00 (UTC+0))
+   * @returns {number} - value of the date.
+   */
   valueOf(): number {
     return this.getTime();
   }
 
+  /**
+   * Get the timezone offset from UTC in minutes.
+   * @returns {number} - timezone offset in minutes.
+   */
   getTimezoneOffset() {
     return this.tzOffset ?? this.d.getTimezoneOffset();
   }
@@ -108,54 +168,117 @@ export default class TZDate {
     return this.d.getTime();
   }
 
+  /**
+   * Get the year of the instance.
+   * @returns {number} - full year
+   */
   getFullYear(): number {
     return this.d.getFullYear();
   }
 
+  /**
+   * Get the month of the instance. (zero-based)
+   * @returns {number} - month
+   */
   getMonth(): number {
     return this.d.getMonth();
   }
 
+  /**
+   * Get the date of the instance.
+   * @returns {number} - date
+   */
   getDate(): number {
     return this.d.getDate();
   }
 
+  /**
+   * Get the hours of the instance.
+   * @returns {number} - hours
+   */
   getHours(): number {
     return this.d.getHours();
   }
 
+  /**
+   * Get the minutes of the instance.
+   * @returns {number} - minutes
+   */
   getMinutes(): number {
     return this.d.getMinutes();
   }
 
+  /**
+   * Get the seconds of the instance.
+   * @returns {number} - seconds
+   */
   getSeconds(): number {
     return this.d.getSeconds();
   }
 
+  /**
+   * Get the milliseconds of the instance.
+   * @returns {number} - milliseconds
+   */
   getMilliseconds(): number {
     return this.d.getMilliseconds();
   }
 
+  /**
+   * Get the day of the week of the instance.
+   * @returns {number} - day of the week
+   */
   getDay(): number {
     return this.d.getDay();
   }
 
+  /**
+   * Sets the instance to the time represented by a number of milliseconds since 1970-01-01 00:00:00 (UTC+0).
+   * @param {number} t - number of milliseconds
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setTime(t: number): number {
     return this.d.setTime(t);
   }
 
+  /**
+   * Sets the year-month-date of the instance. Equivalent to calling `setFullYear` of `Date` object.
+   * @param {number} y - year
+   * @param {number} m - month (zero-based)
+   * @param {number} d - date
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setFullYear(y: number, m = this.getMonth(), d = this.getDate()): number {
     return this.d.setFullYear(y, m, d);
   }
 
+  /**
+   * Sets the month of the instance. Equivalent to calling `setMonth` of `Date` object.
+   * @param {number} m - month (zero-based)
+   * @param {number} d - date
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setMonth(m: number, d = this.getDate()): number {
     return this.d.setMonth(m, d);
   }
 
+  /**
+   * Sets the date of the instance. Equivalent to calling `setDate` of `Date` object.
+   * @param {number} d - date
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setDate(d: number): number {
     return this.d.setDate(d);
   }
 
+  /**
+   * Sets the hours of the instance. Equivalent to calling `setHours` of `Date` object.
+   * @param {number} h - hours
+   * @param {number} M - minutes
+   * @param {number} s - seconds
+   * @param {number} ms - milliseconds
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setHours(
     h: number,
     M = this.getMinutes(),
@@ -165,18 +288,41 @@ export default class TZDate {
     return this.d.setHours(h, M, s, ms);
   }
 
+  /**
+   * Sets the minutes of the instance. Equivalent to calling `setMinutes` of `Date` object.
+   * @param {number} M - minutes
+   * @param {number} s - seconds
+   * @param {number} ms - milliseconds
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setMinutes(M: number, s = this.getSeconds(), ms = this.getMilliseconds()): number {
     return this.d.setMinutes(M, s, ms);
   }
 
+  /**
+   * Sets the seconds of the instance. Equivalent to calling `setSeconds` of `Date` object.
+   * @param {number} s - seconds
+   * @param {number} ms - milliseconds
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setSeconds(s: number, ms = this.getMilliseconds()): number {
     return this.d.setSeconds(s, ms);
   }
 
+  /**
+   * Sets the milliseconds of the instance. Equivalent to calling `setMilliseconds` of `Date` object.
+   * @param {number} ms - milliseconds
+   * @returns {number} - Passed milliseconds of the instance since 1970-01-01 00:00:00 (UTC+0).
+   */
   setMilliseconds(ms: number): number {
     return this.d.setMilliseconds(ms);
   }
 
+  /**
+   * Set the timezone offset of the instance.
+   * @param {string|number} tzValue - The name of timezone(IANA name) or timezone offset(in minutes).
+   * @returns {TZDate} - New instance with the timezone offset.
+   */
   tz(tzValue: string | 'Local' | number) {
     if (tzValue === 'Local') {
       return new TZDate(this.getTime());
@@ -196,6 +342,9 @@ export default class TZDate {
    * the instance is converted to the system timezone.
    *
    * Instance's `tzOffset` property will be ignored if there is a `tzValue` parameter.
+   *
+   * @param {string|number} tzValue - The name of timezone(IANA name) or timezone offset(in minutes).
+   * @returns {TZDate} - New instance with the system timezone.
    */
   local(tzValue?: string | number) {
     if (isPresent(tzValue)) {
