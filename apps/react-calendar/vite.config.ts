ce451eb72580b75d2301ac627e504e8b1c443358
lja1018 <jaeeon.lim@nhn.com>
2022-06-22 17:49:38 +0900

    env: add init config

---
+++ apps/react-calendar/vite.config.ts
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    react({
+      include: '**/*.tsx',
+    }),
+  ],
+});
