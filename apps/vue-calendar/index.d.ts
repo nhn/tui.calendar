877f8c6263b52ade348f4e279dcf85648c368056
곽희나 <heena.kwag@nhn.com>
2022-06-29 22:39:14 +0900

    fix: set typeVersions to link package's multiple entry points

--- apps/vue-calendar/index.d.ts
+++ apps/vue-calendar/index.d.ts
@@ -1,22 +1,13 @@
 import type Vue from 'vue';
 import type Calendar from '@toast-ui/calendar';
 
-declare module '@toast-ui/calendar' {
-  export default class VueCalendar extends Vue {
-    getRootElement(): HTMLDivElement;
-    getInstance(): Calendar; 
-  }
+export default class VueCalendar extends Vue {
+  getRootElement(): HTMLDivElement;
+  getInstance(): Calendar; 
 }
 
-declare module '@toast-ui/calendar/ie11' {
-  export default class VueCalendar extends Vue {
-    getRootElement(): HTMLDivElement;
-    getInstance(): Calendar; 
-  }
-}
-
-declare module '@toast-ui/calendar/esm' {
-  export default class VueCalendar extends Vue {
+declare namespace tui {
+  export class VueCalendar extends Vue {
     getRootElement(): HTMLDivElement;
     getInstance(): Calendar; 
   }
