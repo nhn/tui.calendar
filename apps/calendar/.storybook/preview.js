b8b717ac356923d2d50976c90413316bd3eddbfd
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-06-29 21:06:32 +0900

    test: restore storybook default layout
    * chore: one e2e test affected by 19c56dfe3399fd08019b8305246f3b4fb9597858

--- apps/calendar/.storybook/preview.js
+++ apps/calendar/.storybook/preview.js
@@ -2,3 +2,7 @@ import 'preact/debug';
 import '@src/css/index.css';
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
+
+export const parameters = {
+  layout: 'fullscreen',
+};
