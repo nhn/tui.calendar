768dc43fe29c4ff97a22b93deeabd489ff50dc94
Dohyung Ahn <dohyung.ahn@nhn.com>
2022-07-06 12:41:30 +0900

    feat: export more types from `index.ts`

--- apps/calendar/src/index.ts
+++ apps/calendar/src/index.ts
@@ -4,9 +4,11 @@ import Month from '@src/factory/month';
 import Week from '@src/factory/week';
 import TZDate from '@src/time/date';
 
+import type { ExternalEventTypes } from '@t/eventBus';
 import type { EventObjectWithDefaultValues } from '@t/events';
+import type { Options } from '@t/options';
 
 export default Calendar;
 
 export { Day, Month, TZDate, Week };
-export type { EventObjectWithDefaultValues as EventObject };
+export type { EventObjectWithDefaultValues as EventObject, ExternalEventTypes, Options };
